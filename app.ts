aee75ebe6efaea7ca5f5102fbd743712315aada8
Roan Brand <roan.brand@imqs.co.za>
2015-10-13 16:38:28 +0200

    Please review 2nd time. Made the changes

--- app.ts
+++ app.ts
@@ -1,15 +1,24 @@
 ﻿/// <reference path="Scripts\typings\jquery\jquery.d.ts" />
 
-var maxEntries = 25;	// How many rows should we fit on a page
+var maxEntries;	        // How many rows should we fit on a page
 var curIndex = 0;		// Current index of data displayed in table
 
-/*	Initial page startup	*/
 window.onload = () => {
-	createColumns();	// Retrieve and create Column names
+    createColumns();	// Retrieve and create Column names
+    NumofRows();
 	populateTable();	// Load initial data on page
 };
 
-/*	Browse lower index		*/
+// Run this function when window resizes
+function NumofRows() {
+    var height = $(window).height();
+    maxEntries = Math.round((height-40) / 34);
+    populateTable();
+}
+
+window.onresize = NumofRows;
+
+//  Browse lower index
 var scrollUp = function () {
 	$("#scrollLeft").off();	// Disable button until routine completion
 	curIndex -= maxEntries;
@@ -19,45 +28,54 @@ var scrollUp = function () {
 	populateTable();
 }
 
-/*	Browse higher index		*/
+//	Browse higher index
 var scrollDown = function () {
 	$("#scrollRight").off();
-	$.getJSON("/recordCount", function (numRecords) {	// Upper limit protection
-		if ((curIndex + 2*maxEntries) > (numRecords - 1)) {
-			curIndex = numRecords - 1 - maxEntries;
-		} else {
-			curIndex += maxEntries;
-		}
-		populateTable();
-	});
+    $.getJSON("/recordCount", function (numRecords) {	// Upper limit protection
+        if ((curIndex + 2 * maxEntries) > (numRecords - 1)) {
+            curIndex = numRecords - 1 - maxEntries;
+        } else {
+            curIndex += maxEntries;
+        }
+        populateTable();
+    })
+        .fail(function () {
+            alert("Cannot connect to server!");
+        });
 }
 
-/*	Populate Table given current Index	*/
+//	Populate Table given current Index
 var populateTable = function () {
-	$.getJSON("/records?from=" + curIndex + "&to=" + (curIndex + maxEntries), function (data) {
-		$("#MainTable tbody").empty();
-		for (var i = 0; i < data.length; i++) {
-			insertDataRow(data[i]);
-		}
-	});
+    $.getJSON("/records?from=" + curIndex + "&to=" + (curIndex + maxEntries), function (data) {
+        $("#MainTable tbody").empty();
+        for (var i = 0; i < data.length; i++) {
+            insertDataRow(data[i]);
+        }
+    })
+        .fail(function () {
+            alert("Cannot connect to server!");
+        });
 	$("#currentIndex").html(curIndex + " - " + (curIndex + maxEntries));	// Display current index
 	$("#scrollLeft").off().on('click', scrollUp);	// Bind buttons to navbar, only after data insertion
 	$("#scrollRight").off().on('click', scrollDown);
 }
 
-/*	Create Table columns	*/
+//	Create Table columns
 var createColumns = function () {
-	$.getJSON("/columns", function (data) {
-		var colHeadings = "<tr>";
-		for (var i = 0; i < data.length; i++) {
-			colHeadings += "<th>" + data[i] + "</th>";
-		}
-		colHeadings += "</tr>";
-		$("#MainTable thead").append(colHeadings);
-	});
-}
+    $.getJSON("/columns", function (data) {
+        var colHeadings = "<tr>";
+        for (var i = 0; i < data.length; i++) {
+            colHeadings += "<th>" + data[i] + "</th>";
+        }
+        colHeadings += "</tr>";
+        $("#MainTable thead").append(colHeadings);
+    })
+        .fail(function () {
+            alert("Cannot connect to server!");
+        })
+};
 
-/*	Append single datarow	*/
+//	Append single datarow
 var insertDataRow = function (rowData) {
 	var buildRow = "<tr>";
 	for (var i = 0; i < rowData.length; i++) {
