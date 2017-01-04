c64a2b699f2606c40d8b6f18b878176bf89f3028
Fritz Engelbrecht <FritzE@imqs.local>
2017-01-04 16:17:21 +0200

    Table almost fills the entire window accurately.

--- app.js
+++ app.js
@@ -1,12 +1,69 @@
 /// <reference path="./third_party/jquery.d.ts" />
+var rowNum = 0;
+var table;
+var Table = (function () {
+    function Table() {
+        var table = document.getElementById("mainTable");
+        this.tableHead = table.createTHead();
+        this.tableBody = table.createTBody();
+    }
+    Table.prototype.getHead = function () {
+        return this.tableHead;
+    };
+    Table.prototype.getBody = function () {
+        return this.getBody;
+    };
+    Table.prototype.update = function (data) {
+        var row;
+        for (var i = 0; i < data.length; i++) {
+            row = new Row(this.tableBody, rowNum + i);
+            row.addRow(data[i]);
+        }
+    };
+    return Table;
+}());
+var Row = (function () {
+    function Row(body, index) {
+        this.tableBody = body;
+        this.index = index;
+    }
+    Row.prototype.addRow = function (values) {
+        if (values === void 0) { values = []; }
+        var row = this.tableBody.insertRow(this.index);
+        var cell;
+        for (var i = 0; i < values.length; i++) {
+            cell = row.insertCell(i);
+            cell.innerHTML = values[i];
+        }
+    };
+    return Row;
+}());
+var Headings = (function () {
+    function Headings(head) {
+        this.tableHead = head;
+    }
+    Headings.prototype.makeColumnHeadings = function (headings) {
+        var row = this.tableHead.insertRow(0);
+        var cell;
+        for (var i = 0; i < headings.length; i++) {
+            cell = row.insertCell(i);
+            cell.innerHTML = "<b>" + headings[i] + "<b>";
+        }
+    };
+    return Headings;
+}());
 window.onload = function () {
-    var count;
-    count = document.body.innerHTML;
-    count += "Count = ";
-    $.get("http://localhost:2050/recordCount", function (data) {
-        $("body").append(data);
-        count += data;
+    table = new Table();
+    var tableHead = new Headings(table.getHead());
+    var NumToFetch = 0;
+    NumToFetch = Math.floor(window.innerHeight / 25) - 1;
+    $.getJSON("http://localhost:2050/columns", function (data) {
+        tableHead.makeColumnHeadings(data);
     });
-    document.body.innerHTML = count;
+    $.getJSON("http://localhost:2050/records", { from: rowNum, to: rowNum + NumToFetch }, function (data) {
+        table.update(data);
+    });
+};
+window.onresize = function (event) {
 };
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,15 +1,91 @@
 ﻿/// <reference path="./third_party/jquery.d.ts" />
 
+let rowNum = 0;
+let table: Table;
+
+class Table {
+	tableHead;
+	tableBody;
+
+	constructor() {
+		let table = <HTMLTableElement>document.getElementById("mainTable");
+		this.tableHead = table.createTHead();
+		this.tableBody = table.createTBody();
+	}
+
+	getHead() {
+		return this.tableHead;
+	}
+
+	getBody() {
+		return this.getBody;
+	}
+
+	update(data) {
+		let row;
+		for (let i = 0; i < data.length; i++){
+			row = new Row(this.tableBody, rowNum + i);
+			row.addRow(data[i]);
+		}
+	}
+}
+
+class Row {
+	tableBody;
+	index;
+
+	constructor(body, index) {
+		this.tableBody = body;
+		this.index = index;
+	}
+
+	addRow(values = []) {
+		let row = this.tableBody.insertRow(this.index);
+
+		let cell;
+		for (let i = 0; i < values.length; i++) {
+			cell = row.insertCell(i);
+			cell.innerHTML = values[i];
+		}
+	}
+}
+
+class Headings {
+	tableHead;
+
+	constructor(head) {
+		this.tableHead = head;
+	}
+
+	makeColumnHeadings(headings: string[]) {
+		let row = this.tableHead.insertRow(0);
+
+		let cell;
+		for (let i = 0; i < headings.length; i++) {
+			cell = row.insertCell(i);
+			cell.innerHTML = "<b>" + headings[i] + "<b>";
+		}
+	}
+}
+
 window.onload = () => {
 
-	var count;
-	count = document.body.innerHTML;
-	count += "Count = ";
+	table = new Table();
+	let tableHead = new Headings(table.getHead());
+	let NumToFetch: number = 0;
 
-	$.get("http://localhost:2050/recordCount", function (data) {
-		$("body").append(data);
-		count += data;
+	NumToFetch = Math.floor(window.innerHeight / 25) - 1;
+	
+	$.getJSON("http://localhost:2050/columns", function (data) {
+		tableHead.makeColumnHeadings(data);
 	});
 
-	document.body.innerHTML = count;
+	$.getJSON("http://localhost:2050/records", { from: rowNum, to: rowNum + NumToFetch },
+		function (data) {
+			table.update(data);
+		});	
+};
+
+window.onresize = function (event) {
+    
 };
