de8e32fbb44d8f588f92241c242fd21e68592eb8
Jason George <JasonG@imqs.local>
2019-06-10 16:52:37 +0200

    Implemented some changes from J. Briers and W. Guess - will continue in the morning

--- app.ts
+++ app.ts
@@ -1,171 +1,185 @@
 //#region AppLogic
 //#region API methods
-function getRecordCount() {
-    return fetch('http://localhost:2050/recordCount')
-        .then((response) => {
-            return response.json();
-        });
+async function getRecordCount() : Promise<number> {
+    const response = await fetch('http://localhost:2050/recordCount');
+    return await response.json();
 }
-function getColumnNames() {
-    return fetch('http://localhost:2050/columns')
-        .then((response) => {
-            return response.json();
-        });
+async function getColumnNames() : Promise<string[]>{
+    const response = await fetch('http://localhost:2050/columns');
+    return await response.json();
 }
-async function getRecords(fromID: number, toID: number) {
-    const request = 'http://localhost:2050/records?from=' + (fromID).toString() + '&to=' + (toID).toString();
-    return await fetch(request)
-        .then((response) => {
-            return response.json();
-        });
+async function getRecords(fromID: number, toID: number): Promise<string[][]> {
+    const response = await fetch(`http://localhost:2050/records?from=${(fromID).toString()}&to=${(toID).toString()}`);
+    return await response.json();
 }
 //#endregion
 
 //#region Data Loading methods
 
-function placeRecords(fromID: number, toID: number) {
+function placeRecords(fromID: number, toID: number): number[] {
     getRecords(fromID, toID)
-        .then((records: Array<Array<string>>) => {
+        .then((records: string[][]) => {
             let appendable = '';
             for (const record of records) {
-                 appendable += '<tr id="' + record[0] + '">';
+                 appendable += `<tr id="table-row-${record[0]}">`;
                 for (const column of record) {
-                    appendable += '<td align="center">' + column.toString() + '</td>';     
+                    appendable += `<td align="center">${column.toString()}</td>`;     
                 }
                 appendable += '</tr>';
-                
             }
-            $("#ContentBody").empty();
-            $("#ContentBody").append(appendable);
+            $("#WrapperTable-ContentBody").empty();
+            $("#WrapperTable-ContentBody").append(appendable);
         });
     return [fromID as number, toID as number];
 }
 
 // Credit: https://gist.github.com/ca0v/73a31f57b397606c9813472f7493a940
 
-function placeRecordsFromCursor(cursor: Array<number>) {
+function placeRecordsFromCursor(cursor: number[]): number[] {
     cursor = cursor.sort((a,b) => {return a-b});
-    return placeRecords(cursor[0], cursor[1])
+    return placeRecords(cursor[0], cursor[1]);
 }
 //#endregion
 
 //#region Handlers
-function getPageContent(fromID: number, toID: number) {
-    $("#HeaderRow").empty();
+function getPageContent(fromID: number, toID: number): number[] {
+    $("#WrapperTable-HeaderRow").empty();
     getColumnNames()
-        .then((columns: Array<string>) => {
+        .then((columns: string[]) => {
             for (const column of columns) {
-                let writable: string = '<th align="center">' + column + '</th>';
-                $("#HeaderRow").append(writable);
+                let writable: string = `<th align="center">${column}</th>`;
+                $("#WrapperTable-HeaderRow").append(writable);
             }
         });
-    
     return placeRecords(fromID, toID);
 }
 
-function toNumber(input: string | number, output: any = 1) {
-    if (typeof input === "string") {
-        if (output == 1) {
-            return parseInt(input);
-        } else {
-            return parseFloat(input);
-        }
-    }
-    else if (typeof input === "number") {
-        return input;
-    }
-    else {
-        return 0;
+function toNumber(input: string | number, output: any = 1) : number {
+    switch (typeof input) {
+        case ('string'):
+            if (output == 1) {
+                return parseInt(input as string);
+            } else {
+                return parseFloat(input as string);
+            }
+        case ("number"):
+            return input as number;
+        default:
+            return 0;
     }
 }
-function nextPageResize(previousCursor: Array<number>): number {
-        let fromID = toNumber(previousCursor.sort((a, b) => {return a - b})[0]);
-        let toID = toNumber(previousCursor.sort((a, b) => {return a - b})[1]);
 
-        const documentHeight = ($(window).innerHeight() as number) - ($("#" + fromID.toString()).height() as number);
+function calculateToId(fromId: number): number {
+    let recordDisplayOffset = 0;
+    if (window.innerHeight <= 646) {
+        recordDisplayOffset = 0
+    } else if (window.innerHeight <= 969) {
+        recordDisplayOffset = 1;
+    } else if (window.innerHeight <= 1938) {
+        recordDisplayOffset = 3
+    } else {
+        recordDisplayOffset = 15
+    }
+
+    return recordDisplayOffset + (fromId + Math.floor((window.innerHeight - ($("#form-content").innerHeight() as number)) / 37));
+}
+
+function nextPageResize(previousCursor: number[]): number {
+        const fromID = toNumber(previousCursor.sort((a, b) => {return a - b})[0]);
+        const toID = toNumber(previousCursor.sort((a, b) => {return a - b})[1]);
+        const documentHeight = ($(window).innerHeight() as number) - ($(`#table-row-${fromID.toString()}`).height() as number);
+
         for (let i = fromID; i <= toID; i++) {
-            let element = ($("#" + i.toString()).offset() as JQueryCoordinates).top as number;
+            const elementHeightOffset = ($(`#table-row-${i.toString()}`).offset() as JQueryCoordinates).top as number;
 
-            if ( element >= documentHeight ){
+            if ( elementHeightOffset >= documentHeight ){
                 return i;
             } else {
-                continue
-            };
+                continue;
+            }
         }
-        return toID
+        return toID;
 }
 
-function previousPageResize(previousCursor: Array<number>): Array<number> {
-    let fromId: number = previousCursor[0] - (nextPageResize(previousCursor) - previousCursor[0]);
-    let toId: number = fromId + step;
-    return [previousCursor[0] - (nextPageResize(previousCursor) - previousCursor[0]), toId]
+function previousPageResize(previousCursor: number[]): number[] {
+    const toId = calculateToId(previousCursor[0] - (nextPageResize(previousCursor) - previousCursor[0]));
+    return [previousCursor[0] - (nextPageResize(previousCursor) - previousCursor[0]), toId];
 }
 //#endregion
 //#endregion
 
-const step: number = 37;
-let previousCursor: Array<number>;
+let previousCursor: number[];
 let previousScale: number;
 
 window.onload = () => {     
-    previousCursor = getPageContent(0,37)
+    previousCursor = getPageContent(0, calculateToId(0));
     
     $("#previous-page").click(() => { 
         previousCursor = previousPageResize(previousCursor);
-        let fromId: number = (previousCursor[0] >= 0 ? previousCursor[0] : 0) as number;
-        let toId: number = (previousCursor[0] >= 0 ? previousCursor[1] : 37) as number;
+        let fromId = (previousCursor[0] >= 0 ? previousCursor[0] : 0);
+        const possibleStep = calculateToId(fromId) - fromId;
+        let toId = (previousCursor[0] >= 0 ? previousCursor[1] : possibleStep);
         getRecordCount()
-            .then((value) => {
-                fromId = fromId == value - 1 ? fromId - step : fromId;
-                toId = toId <= value - 1 ? toId : value - 1;
+            .then((recordCount) => {
+                fromId = fromId == recordCount - 1 ? fromId - possibleStep : fromId;
+                toId = toId <= recordCount - 1 ? toId : recordCount - 1;
                 previousCursor = placeRecords(fromId, toId);
             });
         
     });
 
     $("#next-page").click(() => {
-            let fromId: number = nextPageResize(previousCursor) as number;
+            const fromId = nextPageResize(previousCursor);
+            const possibleStep = calculateToId(fromId) - fromId;
             getRecordCount()
-                .then((value) => {
-                    if ( fromId <= value - step - 1 ) {
-                        let toId: number = fromId + step <= value - 1 ? fromId + step : value - 1;
+                .then((recordCount) => {
+                    if ( fromId <= recordCount - possibleStep - 1 ) {
+                        const toId = fromId + possibleStep <= recordCount - 1 ? fromId + possibleStep : recordCount - 1;
                         previousCursor = placeRecords(fromId, toId);
-                    } else if (fromId <= value - 1)  {
-                        previousCursor = placeRecords(fromId, value - 1);
+                    } else if (fromId <= recordCount - 1)  {
+                        previousCursor = placeRecords(recordCount - 1 - (calculateToId(fromId) - fromId), recordCount - 1);
+                        alert('You reached the last record - which is shown at the bottom of the screen')
                     } else {
-                        alert('You have reached the end of the list')
+                        alert('You have reached the end of the list');
                     }
                 });        
         });
 
     $("#go-to-button").click(() => {
-        let fromId = toNumber($("#go-to-index").val() as string, 2);
-        getRecordCount()
-            .then((value) => {
+        const fromId = toNumber($("#go-to-index").val() as string, 2);
+        const possibleStep = calculateToId(fromId) - fromId;
+        if (fromId < 0){
+            alert('You may only insert Id greater than or equal to 0');
+        } else {
+            getRecordCount()
+            .then((recordCount) => {
                 if (Math.floor(fromId).toString() == fromId.toString() === true) {
-                    if ( fromId > value - step ) {
-                        alert('You may not insert a value greater than ' + (value - step).toString());
+                    if ( fromId > recordCount - possibleStep ) {
+                        alert(`You may not insert a desired Id greater than ${(recordCount - possibleStep).toString()}`);
                     } else {
-                        let toId: number = (fromId as number) + step < value ? (fromId as number) + step : value - 1;
+                        let toId = (fromId) + possibleStep < recordCount ? (fromId) + possibleStep : recordCount - 1;
                         previousCursor = placeRecords(fromId, toId);
                     }
                 } else {
                     alert('It seems you are not inserting an integer - please ensure that you are.');
                 }
             });
+        }
     });
 }
-let resizeTimer: number = 50;
-$(window).on('resize', (e) => {
-
+let resizeTimer = 50;
+window.onresize = () => {
+    const x = calculateToId(previousCursor[0]);
     clearTimeout(resizeTimer);
     resizeTimer = setTimeout(() => {
-    getRecordCount()
-        .then((value) => {   
-            if ( previousCursor[1] == value - 1 ) {
-                previousCursor = placeRecords(previousCursor[1]- step, previousCursor[1]);
-            }
-            $('#body').height(5000 * previousScale);
-        })
-    }, 250)
-});
+        getRecordCount()
+            .then((recordCount) => {   
+                if ( x >= recordCount - 1 ) {
+                    previousCursor = placeRecords(recordCount - 1 - (calculateToId(previousCursor[0]) - previousCursor[0]), recordCount - 1);
+                    alert('Note that since you were on the last page, the final record is still at the bottom of your page')
+                } else {
+                    previousCursor = placeRecords(previousCursor[0], x)
+                }
+            })
+    }, 250);
+}
