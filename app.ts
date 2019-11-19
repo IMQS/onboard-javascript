2c93272220f026fa00d8e1f48dad823c793c7e67
Helaine <heliduplessis@gmail.com>
2019-11-19 14:53:18 +0200

    Code review changes 2

--- app.ts
+++ app.ts
@@ -1,94 +1,85 @@
 "use strict";
-window.onload = () => { onloadValues() };
-window.onresize = () => { fromAndToValuesAdjustment() };
+window.onload = () => { onLoadValues() };
+window.onresize = () => {
+	const adjustValues = () => {
+		fromAndToValuesAdjustment()
+	}
+	clearTimeout(timeout);
+	timeout = setTimeout(adjustValues, 200);
+};
 
-const rowHeight = 62;
+let timeout: number;
+const ROWHEIGHT = 64;
 let from = 0;
 let to = 0;
-let fittableRows = 0;
 
-/**
+/**callHeaders
  * calculates the amount of rows that can fit on the screen given its current height
 */
-function amountOfRowsThatFit() {
-
-	let buttonEl = (<HTMLInputElement>document.getElementById("previousPage"))
-
-	let windowHeigt = window.innerHeight;
-	let buttonDivheigth = 67;
-	let adaptedHeight = windowHeigt - buttonDivheigth;
-
-	return Math.floor((adaptedHeight / rowHeight) - 1);
+function eligableRowCount() {
+	let windowHeight = window.innerHeight;
+	let buttonDivHeight = (<HTMLInputElement>document.getElementById("buttons")).offsetHeight;
+	let adaptedHeight = windowHeight - buttonDivHeight;
+	console.log(Math.floor((adaptedHeight / ROWHEIGHT) - 1));
+	return Math.floor((adaptedHeight / ROWHEIGHT) - 1);
 
 }
-
 /**
 *These are the starting values for when the screen loads
 */
-function onloadValues() {
+function onLoadValues() {
 	let toValue = (<HTMLInputElement>document.getElementById("toValue")).value;
 	let fromValue = (<HTMLInputElement>document.getElementById("fromValue")).value;
 
 	from = 0;
 	fromValue = (<HTMLInputElement>document.getElementById("fromValue")).value = from.toString();
-	let rowsFittable = amountOfRowsThatFit();
+	let rowsFittable = eligableRowCount();
 	to = from + rowsFittable;
 	toValue = (<HTMLInputElement>document.getElementById("toValue")).value = to.toString();
-	callHeaders(from, to);
+	retrieveHeaders(from, to);
 }
-
 /**
  *Adjust the values in the start and end boxes according to the screen heigth whenever it resizes
  */
 function fromAndToValuesAdjustment() {
-	var timeout;
 	if (timeout) clearTimeout(timeout);
 	timeout = setTimeout(() => {
 		let toValue = (<HTMLInputElement>document.getElementById("toValue")).value;
 		let fromValue = (<HTMLInputElement>document.getElementById("fromValue")).value;
 
 		from = parseInt(fromValue);
-		let rowsThatFit = amountOfRowsThatFit();
+		let rowsThatFit = eligableRowCount();
 		to = from + rowsThatFit;
 
 		if (to > 100000) {
-			window.alert("Nee, stout >.<,your search has exceeded the maximum rows")
 			from = 100000 - rowsThatFit;
 			to = 100000;
 		} else if (from < 0) {
-			window.alert("Nee, stout >.<,your search has exceeded the minimum rows")
 			from = 0;
 			to = from + rowsThatFit;
 		}
 		fromValue = (<HTMLInputElement>document.getElementById("fromValue")).value = from.toString();
 		toValue = (<HTMLInputElement>document.getElementById("toValue")).value = to.toString();
-		callHeaders(from, to);
-	}, 200);
+		retrieveHeaders(from, to);
+	});
 }
 /**
  *Executes when the user presses the next button
  */
 function pressedNext() {
-
 	let toValue = (<HTMLInputElement>document.getElementById("toValue")).value;
 	let fromValue = (<HTMLInputElement>document.getElementById("fromValue")).value;
-
-	let rowsThatFit = amountOfRowsThatFit();
-
+	let rowsThatFit = eligableRowCount();
 	from = parseInt(toValue) + 1;
 	to = from + rowsThatFit;
 
 	if (to > 100000) {
-		window.alert("Nee, stout >.<,your search has exceeded the maximum rows")
 		from = 100000 - rowsThatFit;
-
 		to = 100000;
 	}
 	fromValue = (<HTMLInputElement>document.getElementById("fromValue")).value = from.toString();
 	toValue = (<HTMLInputElement>document.getElementById("toValue")).value = to.toString();
-
-
-	callHeaders(from, to);
+	retrieveHeaders(from, to);
 }
 /**
  *Executes when the user presses the previous button
@@ -97,26 +88,23 @@ function pressedPrevious() {
 
 	let toValue = (<HTMLInputElement>document.getElementById("toValue")).value;
 	let fromValue = (<HTMLInputElement>document.getElementById("fromValue")).value;
-	let rowsThatFit = amountOfRowsThatFit();
+	let rowsThatFit = eligableRowCount();
 
 	to = from - 1;
 	from = parseInt(fromValue) - rowsThatFit;
 	if (from <= -1) {
-		window.alert("Nee, stout >.<,your search has exceeded the minimum rows")
 		from = 0;
 		to = from + rowsThatFit;
 	}
 	fromValue = (<HTMLInputElement>document.getElementById("fromValue")).value = from.toString();
 	toValue = (<HTMLInputElement>document.getElementById("toValue")).value = to.toString();
-	callHeaders(from, to);
+	retrieveHeaders(from, to);
 
 }
-
-
 /**
  *Retrieves column names
 */
-function callHeaders(from: number, to: number) {
+function retrieveHeaders(from: number, to: number) {
 	fetch('http://localhost:2050/columns')
 		.then((resp) => {
 			return resp.json();
@@ -127,7 +115,6 @@ function callHeaders(from: number, to: number) {
 			retrieveRows(from, to);
 		});
 }
-
 /**
  *Retrieves the table rows with data
  */
@@ -146,23 +133,19 @@ function retrieveRows(from: number, to: number) {
 
 			columnData.forEach(function (item) {
 				let newRow: HTMLElement = document.createElement('tr');
-				newRow.setAttribute('id', 'rowNumber' + j);
+				newRow.setAttribute('id', 'row-number-' + j);
 				document.getElementById("intialTable")!.appendChild(newRow);
 				let content = Array.from(item);
 				content.forEach(function (index) {
 					let columnName = document.createElement('td');
 					columnName.textContent = index;
-					document.getElementById("rowNumber" + j)!.appendChild(columnName);
+					document.getElementById("row-number-" + j)!.appendChild(columnName);
 
 					indexNumber++;
 				});
 				j++;
 			});
 		});
-
-
-
-
 }
 
 function create(input: string[]) {
@@ -176,13 +159,13 @@ function create(input: string[]) {
 	document.getElementById("body")!.appendChild(table);
 
 	let newRow = document.createElement('tr');
-	newRow.setAttribute('id', 'rowNumber0');
+	newRow.setAttribute('id', 'row-number-0');
 	document.getElementById("intialTable")!.appendChild(newRow);
 
 	for (let i = 0; i < input.length; i++) {
 		let columnName = document.createElement('th');
 		columnName.textContent = input[i]
 		columnName.setAttribute('id', 'th');
-		document.getElementById("rowNumber0")!.appendChild(columnName);
+		document.getElementById("row-number-0")!.appendChild(columnName);
 	}
 }
