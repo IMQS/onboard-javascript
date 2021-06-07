a70d63420e016e875b260ea8a46989c7eaf144d7
gerald.tivatyi <geraldtivatyi@gmail.com>
2021-06-07 16:44:07 +0200

    Make http request to get data from server. Convert data to be usable for functions.

--- app.ts
+++ app.ts
@@ -2,14 +2,8 @@ import { tableHeadings } from "./classes/table_headings.js";
 import { recordStruct } from "./classes/record_struct.js";
 import { createTable } from "./classes/create_table.js";
 import { HasFormatMethod } from "./interfaces/hasformatmethod.js";
-import { httpReq } from './classes/http';
+import { request } from './httpreq.js';
 
-const request = new httpReq('www.google.com','/','get');
-// request.addArg('num', '1');
-
-// request.get((res: string): void => {
-//     console.log('Response received: ' + res);
-// });
 
 //Accessing Form Data
 const form = document.querySelector('.navigation') as HTMLFormElement;
@@ -17,10 +11,46 @@ const form = document.querySelector('.navigation') as HTMLFormElement;
 const fromID = document.querySelector('#fromID') as HTMLInputElement;
 const toID = document.querySelector('#toID') as HTMLInputElement;
 
-
 // Instantiate grid div
 const table = document.querySelector('table')!;
 const tble = new createTable(table);
+
+// Fetch headings from server
+let getHeadings: string[];
+request(
+	"/columns", "get",
+	function(r: any) {
+		let d: string;
+		let e: string[];
+		try {
+			const myArr = JSON.parse(r);
+			getHeadings = myArr;
+		} catch(err) {
+			console.log(err);
+			return;
+		}
+	}
+);
+
+// Fetch records from server
+let getRecords: string[][];
+request(
+	"/records?from=1&to=5", "get",
+	function(r: any) {
+		let d = {};
+		try {
+			if (r === []) {
+				d = r;
+			} else {
+				getRecords = r;
+				console.log(typeof getRecords);
+			}
+		} catch(err) {
+			console.log(err);
+			return;
+		}
+	}
+);
 
 form.addEventListener('submit', (e: Event) => {
 	document.getElementById("table")!.innerHTML = "";
@@ -29,12 +59,11 @@ form.addEventListener('submit', (e: Event) => {
 	let interfaceHeading: HasFormatMethod;
 	let interfaceRecords: HasFormatMethod;
 
-	// Dummy data
-	const recHeadingsFromServer: string[] = [ "ID", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" ];
-
-	// Instantiate class tableHeadings
+	let recHeadingsFromServer: string[] = getHeadings;
 	interfaceHeading = new tableHeadings(recHeadingsFromServer[0], recHeadingsFromServer[1], recHeadingsFromServer[2], recHeadingsFromServer[3], recHeadingsFromServer[4], recHeadingsFromServer[5], recHeadingsFromServer[6], recHeadingsFromServer[7], recHeadingsFromServer[8], recHeadingsFromServer[9], recHeadingsFromServer[10]);
 
+	// const recFromServer: string[][] = getRecords;
+	
 	// Dummy data
 	const recFromServer: string[][] = [ [ "1", "A1-FqM=", "B1-W5U=", "C1-q4Y=", "D1-M6E=", "E1-IiQ=", "F1-3q4=", "G1-e3c=", "H1-i/c=", "I1-woE=", "J1-ST0=" ], [ "2", "A2-yDY=", "B2-q9s=", "C2-loU=", "D2-ImU=", "E2-cTo=", "F2-ji0=", "G2-SMY=", "H2-rho=", "I2-Ym0=", "J2-RUc=" ], [ "3", "A3-E5g=", "B3-DNI=", "C3-iiE=", "D3-vtc=", "E3-GDQ=", "F3-fJY=", "G3-DfA=", "H3-sUw=", "I3-Cas=", "J3-gE8=" ], [ "4", "A4-ToA=", "B4-I58=", "C4-CNE=", "D4-CA8=", "E4-3nk=", "F4-AVs=", "G4-l4Q=", "H4-Qm4=", "I4-c58=", "J4-6gI=" ], [ "5", "A5-6mQ=", "B5-W6I=", "C5-HZY=", "D5-Eqc=", "E5-7wk=", "F5-bNI=", "G5-RBg=", "H5-Jm4=", "I5-T7s=", "J5-CFo=" ] ];
 	tble.renderHeading(interfaceHeading);
---
+++ httpreq.js
@@ -0,0 +1,14 @@
+export function request(action, method, callback) {
+    var x = {};
+    if (window.XMLHttpRequest)
+        x = new XMLHttpRequest();
+    // else x = new ActiveXObject('Microsoft.XMLHTTP');
+    x.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(this.responseText);
+        }
+    };
+    x.open(method, action, true);
+    x.send();
+}
+//# sourceMappingURL=httpreq.js.map
