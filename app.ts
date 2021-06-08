59f3d00c0c539fe61ed76a0c9f1c0bb7000cb447
geraldtivatyi <geraldtivatyi@gmail.com>
2021-06-08 12:11:09 +0200

    Add styling and fix bug

--- app.ts
+++ app.ts
@@ -19,16 +19,12 @@ let getHeadings: string[];
 let recFromServer: string[][];
 
 function generateTable(){
-	console.log(fromID.valueAsNumber);
-	console.log(toID.valueAsNumber);
-	console.log(recFromServer);
-	console.log(recFromServer[0][1]);
 	let interfaceHeading: HasFormatMethod;
 	let interfaceRecords: HasFormatMethod;
 	let recHeadingsFromServer: string[] = getHeadings;
 	interfaceHeading = new tableHeadings(recHeadingsFromServer[0], recHeadingsFromServer[1], recHeadingsFromServer[2], recHeadingsFromServer[3], recHeadingsFromServer[4], recHeadingsFromServer[5], recHeadingsFromServer[6], recHeadingsFromServer[7], recHeadingsFromServer[8], recHeadingsFromServer[9], recHeadingsFromServer[10]);
 	tble.renderHeading(interfaceHeading);
-	for(let i=fromID.valueAsNumber-1;i<toID.valueAsNumber;i++) {
+	for(let i=0;i<recFromServer.length;i++) {
 		interfaceRecords = new recordStruct(recFromServer[i][0], recFromServer[i][1], recFromServer[i][2], recFromServer[i][3], recFromServer[i][4], recFromServer[i][5], recFromServer[i][6], recFromServer[i][7], recFromServer[i][8], recFromServer[i][9], recFromServer[i][10]);
 		tble.renderRecords(interfaceRecords);
 	}
@@ -67,7 +63,6 @@ form.addEventListener('submit', (e: Event) => {
 				} else {
 					const myArr = JSON.parse(r);
 					recFromServer = myArr;
-					console.log(recFromServer);
 					generateTable();
 				}
 			} catch(err) {
