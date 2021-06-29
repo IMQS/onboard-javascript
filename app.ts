2ca2f32117703fbadbe29f124fa41a18eca94583
gerald.tivatyi <geraldtivatyi@gmail.com>
2021-06-29 18:45:50 +0200

    Updates

--- app.ts
+++ app.ts
@@ -1,10 +1,6 @@
-// Global Variables
-let screenHeight = screen.height;								
-let numOfRows = getNumOfRows();		// Call function to get number of rows to render based on current screen size
-let totalNumofRecords: number;						
-let clickTimeout: ReturnType<typeof setTimeout>;	
+// let totalNumofRecords: number;							
 let fromID = 1;										
-let toID = numOfRows;								
+let toID = getNumOfRows();								
 
 // Get list of headings
 fetch("/columns").then(res => res.text()).then((headingsStr) => {
@@ -15,20 +11,30 @@ fetch("/columns").then(res => res.text()).then((headingsStr) => {
 	hd.constructTableHeadings(interfaceHeading);					// Call method to render table headings element in browser
 }).catch(err => console.log(err));
 
-// Create initial page
-createInitialPage(numOfRows);
+
+fetch("/recordCount").then(res => res.text()).then(
+	(value) => {
+		let t = Number(value) - 1;
+		// Create initial page
+		createInitialPage(getNumOfRows(), t);
+		let numofrecords = document.querySelector('#numofrecords') as HTMLParagraphElement;
+		numofrecords.innerHTML = value; 
+	}
+).catch(err => console.log(err));
 
 // Listen for change in window size
 window.addEventListener('resize', () => {
-	screenHeight = window.innerHeight;	
-	numOfRows = getNumOfRows();	
-
-	// Get records from backend
-	getRecords(fromID, toID);
+	let numofrecords = document.querySelector('#numofrecords') as HTMLParagraphElement;
+	let totalNumofRecords = Number(numofrecords.innerHTML);
+	console.log(totalNumofRecords);
+	getRecords(fromID, toID, totalNumofRecords);
 });
 
 // Listen for clicks on left arrow
 $( "#leftarrow" ).on( "click", () => {
+	let numofrecords = document.querySelector('#numofrecords') as HTMLParagraphElement;
+	let totalNumofRecords = Number(numofrecords.nodeValue);
+	let numOfRows = getNumOfRows();
 	if (fromID < (numOfRows-1) && fromID > 1 || fromID === 1) {
 		fromID = 1;
 		toID = numOfRows;
@@ -38,11 +44,14 @@ $( "#leftarrow" ).on( "click", () => {
 	}
 
 	// Get records from backend
-	getRecords(fromID, toID);
+	getRecords(fromID, toID, totalNumofRecords);
 });
 					
 // Listen for clicks on right arrow
 $( "#rightarrow" ).on( "click", () => {
+	let numofrecords = document.querySelector('#numofrecords') as HTMLParagraphElement;
+	let totalNumofRecords = Number(numofrecords.nodeValue);
+	let numOfRows = getNumOfRows();
 	if (fromID > (totalNumofRecords-((numOfRows-1)*2)) && fromID < totalNumofRecords || toID === totalNumofRecords) { 	
 		fromID = (totalNumofRecords-(numOfRows-1));
 		toID = totalNumofRecords;
@@ -52,13 +61,16 @@ $( "#rightarrow" ).on( "click", () => {
 	}
 	
 	// Get records from backend
-	getRecords(fromID, toID);
+	getRecords(fromID, toID, totalNumofRecords);
 });
 							
 // Listen for submission in form and use inputs to request data from backend
 $( "#submit" ).on( "click", () => {
+	let numofrecords = document.querySelector('#numofrecords') as HTMLParagraphElement;
+	let totalNumofRecords = Number(numofrecords.nodeValue);
+	let numOfRows = getNumOfRows();
 	// Clear the timeout every time if you submit on the form
-	clearTimeout(clickTimeout);
+	clearTimeout(clickTimeout(fromID, toID));
 
 	// Clear form value in from end when navigating data using the arrows
 	let startFromIDElement = document.querySelector('#startfrom') as HTMLInputElement;
@@ -77,43 +89,48 @@ $( "#submit" ).on( "click", () => {
 	toID = fromID + (numOfRows-1);
 
 	// Get records from backend
-	getRecords(fromID, toID);
+	getRecords(fromID, toID, totalNumofRecords);
 });
 
 // Listen for click to go to start of data
 $( "#gotostart" ).on( "click", () => {
+	let numofrecords = document.querySelector('#numofrecords') as HTMLParagraphElement;
+	let totalNumofRecords = Number(numofrecords.nodeValue);
+	let numOfRows = getNumOfRows();
 	// Clear the timeout every time if you click to go to start of dataset
-	clearTimeout(clickTimeout);
+	clearTimeout(clickTimeout(fromID, toID));
 
 	// Set ID values to the start of the dataset
 	fromID = 1;
 	toID = numOfRows;
 
 	// Get records from backend
-	getRecords(fromID, toID);
+	getRecords(fromID, toID, totalNumofRecords);
 });
 
 // Listen for sclick to go to end of data
 $( "#gotoend" ).on( "click", () => {
+	let numofrecords = document.querySelector('#numofrecords') as HTMLParagraphElement;
+	let totalNumofRecords = Number(numofrecords.nodeValue);
+	let numOfRows = getNumOfRows();
 	// Clear the timeout every time if you click to go to end of dataset
-	clearTimeout(clickTimeout);
+	clearTimeout(clickTimeout(fromID, toID));
 
 	// Set ID values to the end of the dataset
 	fromID = totalNumofRecords-(numOfRows-1);
 	toID = totalNumofRecords;
 	
 	// Get records from backend
-	getRecords(fromID, toID);
+	getRecords(fromID, toID, totalNumofRecords);
 });
 
 // Function to create the initial table when loading the page for the first time or when the window size changes
-function createInitialPage(numOfRows: number) {
-	let numofrecords = document.querySelector('#numofrecords') as HTMLParagraphElement;		
+function createInitialPage(numOfRows: number, totalNumofRecords: number) {
 	let fromIDElement = document.querySelector('#fromID') as HTMLParagraphElement;			
 	let toIDElement = document.querySelector('#toID') as HTMLParagraphElement;				
 
 	// Clear the timeout every time this function is called
-	clearTimeout(clickTimeout);
+	clearTimeout(clickTimeout(fromID, toID));
 
 	// Check if you're at the start or end of the data set, and reset IDs for change in window size
 	if (fromID > (totalNumofRecords-(numOfRows-1))) {
@@ -126,25 +143,18 @@ function createInitialPage(numOfRows: number) {
 	fromIDElement.innerHTML = fromID.toString();
 	toIDElement.innerHTML = toID.toString();
 
-	//Request for and set the total number of records
-	fetch("/recordCount").then(res => res.text()).then((value) => {
-		numofrecords.innerHTML = value;
-		totalNumofRecords = Number(value) - 1;
-	}).catch(err => console.log(err));
-
 	// Get records from backend
-	getRecords(fromID, toID);
+	getRecords(fromID, toID, totalNumofRecords);
 }
 
 // Function returning number of rows based on window size
 function getNumOfRows() {
-	// Get height of screen
-	screenHeight = window.innerHeight; 
-	return Math.round(screenHeight/33);
+	return Math.round(window.innerHeight/33);
 }
 
 // Function returning number of rows based on window size
-function getRecords(fromID: number, toID: number) {
+function getRecords(fromID: number, toID: number, totalNumofRecords: number) {
+	let numOfRows = getNumOfRows();
 	let fromIDElement = document.querySelector('#fromID') as HTMLParagraphElement;			
 	let toIDElement = document.querySelector('#toID') as HTMLParagraphElement;				
 
@@ -162,15 +172,35 @@ function getRecords(fromID: number, toID: number) {
 
 	// Clear the timeout every time if you navigate and you're not at the start ID of all the data
 	if (fromID != 1 || toID != totalNumofRecords) {
-		clearTimeout(clickTimeout);
+		clearTimeout(clickTimeout(fromID, toID));
 	}
 
 	// Create table using the new set IDs inside a set timeout
-	clickTimeout = setTimeout( () => {
-		let startfrom = document.querySelector("#startfrom") as HTMLInputElement;
-        startfrom.value = "";
+	clickTimeout(fromID, toID);
+	// clickTimeout = setTimeout( () => {
+	// 	let startfrom = document.querySelector("#startfrom") as HTMLInputElement;
+    //     startfrom.value = "";
 
-        fetch("/records?from="+fromID+"&to="+toID).then(res => res.text()).then((recordsStr) => {
+    //     fetch("/records?from="+fromID+"&to="+toID).then(res => res.text()).then((recordsStr) => {
+	// 		// Empty the table in the html
+	// 		let emptyTable = document.querySelector('#records') as HTMLDivElement;
+	// 		emptyTable.innerHTML = "";												// Clear table to empty html
+	
+	// 		// Recreate table with new headings and records data
+	// 		let interfaceRecords: HasFormatMethod;									// Variable of type interface used in creating table rows
+	// 		interfaceRecords = new RenderTableRows(recordsStr);						// Call method to generate string containing table row elements
+    //     }).catch(err => console.log(err));
+	// }, 200);
+
+	
+}
+
+function clickTimeout(fromID: number, toID: number) {
+	let timeOut = setTimeout( () => {
+		let startfrom = document.querySelector("#startfrom") as HTMLInputElement;
+		startfrom.value = "";
+
+		fetch("/records?from="+fromID+"&to="+toID).then(res => res.text()).then((recordsStr) => {
 			// Empty the table in the html
 			let emptyTable = document.querySelector('#records') as HTMLDivElement;
 			emptyTable.innerHTML = "";												// Clear table to empty html
@@ -178,8 +208,9 @@ function getRecords(fromID: number, toID: number) {
 			// Recreate table with new headings and records data
 			let interfaceRecords: HasFormatMethod;									// Variable of type interface used in creating table rows
 			interfaceRecords = new RenderTableRows(recordsStr);						// Call method to generate string containing table row elements
-        }).catch(err => console.log(err));
+		}).catch(err => console.log(err));
 	}, 200);
+	return timeOut;
 }
 
 class RenderTableRows implements HasFormatMethod {
