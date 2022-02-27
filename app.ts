0fada8668f95a82963de49704b212be194c953fa
CelesteNaude <286celeste@gmail.com>
2022-02-27 10:58:09 +0200

    edited user interface

--- app.ts
+++ app.ts
@@ -2,39 +2,39 @@
 let state = {
 	'page': 1,
 	'records': 15,
-
 	'window': 10
 }
 
+// Search ID function
 function searchFunction() {
 	// Get entered ID and calculate what page it is on
 	let id = Number($('#id-search').val())
 	let pageNum = Math.ceil((id + 1) / state.records)
-	console.log("Go to page ", pageNum)
+
 	// Set page to the one that the searched ID is on
 	state.page = pageNum
 	loadIntoTable(document.querySelector("table"))
 }
 
-
+// Select num records to display from drop down function
 function dropFunction() {
+	// Get value selected in drop down and reload table
 	let num = Number($('#show-by').val())
 	state.records = num
-	console.log("Show records of ", state.records)
 	loadIntoTable(document.querySelector("table"))
 }
 
-
+// Records to display in table function
 async function pagination(tableData: any, numRecords: number, page: number, records: number) {
-	console.log("In pag: ", numRecords, " and ", page, " and ", records)
+	// Calculate where records to display should start and end
 	let trimStart = (page - 1) * records
 	let trimEnd = trimStart + records
-	console.log("In pag: trimStart", trimStart, " and trimEnd", trimEnd, " records? ", records)
 
 	let trimmedData = tableData.slice(trimStart, trimEnd)
 	let pages = Math.ceil(numRecords / records)
 
 	// User info: Display number of records and pages on window
+	// TODO: create a global get element function
 	let pageDet = document.getElementById('page-details')
 	pageDet!.innerHTML = `<p>There are <strong>${numRecords + 1}</strong> records and <strong>${pages}</strong> pages</p>`
 
@@ -44,10 +44,17 @@ async function pagination(tableData: any, numRecords: number, page: number, reco
 	}
 }
 
+// Create paging buttons functions
 function pageButtons(pages: number) {
 	// User info: Display current page number
 	let pageNum = document.getElementById('page-number')
-	pageNum!.innerHTML = `<p>Page ${state.page}</p>`
+	if (state.page) {
+		pageNum!.innerHTML = `<p>Page ${state.page}</p>`
+	}
+	else {
+		pageNum!.innerHTML = `<p>No records to display</p>`
+	}
+
 
 	// Select element to create pagination buttons in
 	let wrapper = document.getElementById('pagination-wrapper')
@@ -62,7 +69,7 @@ function pageButtons(pages: number) {
 		maxRight = state.window
 	}
 
-	// Button numbers cannot exceed number of pages
+	// Button numbers cannot exceed max number of pages
 	if (maxRight > pages) {
 		maxLeft = pages - (state.window - 1)
 		maxRight = pages
@@ -72,6 +79,7 @@ function pageButtons(pages: number) {
 		}
 	}
 
+	// Create buttons
 	for (let page = maxLeft; page <= maxRight; page++) {
 		wrapper!.innerHTML += `<button value=${page} class="page btn">${page}</button>`
 	}
@@ -95,12 +103,17 @@ function pageButtons(pages: number) {
 	})
 }
 
-
+// Load json data into table function
 async function loadIntoTable(table: any) {
+	// Display loader
+	$(".content").fadeOut(500);
+	$(".loader").fadeIn(500);
+
 	// Select table elements to populate
 	let tableHead = table.querySelector("thead")
 	let tableBody = table.querySelector("tbody")
 
+	// API calls for record amount and data
 	let numRecords = await (await fetch('/recordCount')).json()
 	let cnumRecords = numRecords - 1
 	let recordsLink = "/records?from=0&to=" + cnumRecords
@@ -140,6 +153,11 @@ async function loadIntoTable(table: any) {
 		tableBody.appendChild(rowElement)
 	}
 	pageButtons((await rows).pages)
+
+	// Display content
+	console.log("page is finally fully loaded");
+	$(".loader").fadeOut(500);
+	$(".content").fadeIn(500);
 }
 
 window.onload = () => {
