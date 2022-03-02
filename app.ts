dce556753f182374f80880e11c1017f780c350af
CelesteNaude <286celeste@gmail.com>
2022-03-02 21:20:00 +0200

    fixed table responsiveness errors

--- app.ts
+++ app.ts
@@ -1,15 +1,15 @@
 
-const state = {
+let state = {
 	'page': 1,
-	'records': Math.floor((window.innerHeight - 200) / 40),
+	'records': Math.floor((window.innerHeight - 160) / 40),
 	'window': 10,
 	'trimStart': 0,
-	'trimEnd': 10
+	'trimEnd': 10,
+	'countRec': 0
 }
 
 let RECORDCOUNT = 350
 let HEADERS = ["ID", "City", "Population"]
-let timeOutFunctionId: any
 
 function el(element: string) {
 	return document.getElementById(element)
@@ -19,93 +19,11 @@ async function getData() {
 	// API calls for record count and headers
 	RECORDCOUNT = await (await fetch('/recordCount')).json()
 	HEADERS = await (await fetch('/columns')).json()
+
+	// Populate table with fetched data
 	loadIntoTable(document.querySelector("table"))
-	console.log("The height of the window is: ", self.innerHeight)
 }
 
-
-// Bind to the resize event of the window object
-window.addEventListener('resize', function () {
-	let newHeight = Math.floor((window.innerHeight - 200) / 40);
-	let diff = Math.ceil(newHeight - state.records)
-
-	console.log("Must be: ", newHeight)
-	console.log("Currently is: ", state.records)
-
-	let table = document.querySelector("table")
-	let tableBody = table?.querySelector("tbody")
-
-	// state.records = newHeight
-	let pages = Math.ceil(RECORDCOUNT / state.records)
-	pageButtons(pages)
-
-	if (diff < 0) {
-		// Delete rows
-		console.log("Delete ", diff, " rows")
-
-		console.log("Current record start: ", state.trimStart, " and end: ", state.trimEnd)
-
-		deleteRows(state.records, diff)
-
-		state.trimEnd = state.trimEnd + diff
-		console.log("After delete record start: ", state.trimStart, " and end: ", state.trimEnd)
-		state.records = newHeight
-	}
-	else if (diff > 0) {
-		// Add rows
-		console.log("Add ", diff, " rows")
-
-		state.records = newHeight
-
-		console.log("Current record start: ", state.trimStart, " and end: ", state.trimEnd)
-		let start = state.trimEnd + 1
-		let end = state.trimEnd + diff
-		console.log("After record start: ", state.trimStart, " and end: ", end)
-
-		let recordsLink = "/records?from=" + start + "&to=" + end
-
-		addRows(recordsLink)
-
-		state.trimEnd = state.trimEnd + diff
-
-
-	}
-	else {
-		console.log("Do nothing")
-	}
-
-})
-
-async function addRows(link: string) {
-	let table = document.querySelector("table")
-	let tableBody = table?.querySelector("tbody")
-	let data = await (await fetch(link)).json();
-	for (let row of data) {
-		let rowElement = document.createElement("tr")
-
-		for (let cellText of row) {
-			let cellElement = document.createElement("td")
-
-			cellElement.textContent = cellText
-			rowElement.appendChild(cellElement);
-		}
-		tableBody?.appendChild(rowElement)
-	}
-}
-
-async function deleteRows(newHeight: number, diff: number) {
-	let table = document.querySelector("table")
-	let tableBody = table?.querySelector("tbody")
-	let num = newHeight - 1
-
-	console.log("start at ", newHeight)
-
-	for (let i = num; i > (num + diff); i--) {
-		console.log("deleted a row")
-		tableBody!.deleteRow(i)
-	}
-
-}
 
 // Search ID function
 async function searchFunction() {
@@ -120,7 +38,8 @@ async function searchFunction() {
 	}
 	else {
 		// Use entered ID to calculate what page it is on
-		let pageNum = Math.ceil((id + 1) / state.records)
+		console.log("Records for Id: ", state.records)
+		let pageNum = Math.floor(id / state.records + 1)
 
 		// Set page to the one that the searched ID is on
 		state.page = pageNum
@@ -129,11 +48,43 @@ async function searchFunction() {
 	}
 }
 
+// Add rows to table
+async function addRows(link: string) {
+	let table = document.querySelector("table")
+	let tableBody = table?.querySelector("tbody")
+	let data = await (await fetch(link)).json();
+	for (let row of data) {
+		let rowElement = document.createElement("tr")
+		state.countRec += 1
+
+		for (let cellText of row) {
+			let cellElement = document.createElement("td")
+
+			cellElement.textContent = cellText
+			rowElement.appendChild(cellElement);
+		}
+		tableBody?.appendChild(rowElement)
+	}
+}
+
+// Delete rows from table
+async function deleteRows(newHeight: number, diff: number) {
+	let table = document.querySelector("table")
+	let tableBody = table?.querySelector("tbody")
+	let num = newHeight - 1
+
+	for (let i = num; i > (num + diff); i--) {
+		console.log("deleted a row")
+		tableBody!.deleteRow(i)
+	}
+
+}
+
 // Records to display in table function
 async function pagination(recordCount: number, page: number, records: number) {
-	let pages = Math.ceil(recordCount / records)
+	let pages = Math.ceil((recordCount + 1) / records)
 
-	// Calculate where records to display should start and end
+	// Calculate where displayed records should start and end
 	state.trimStart = (page - 1) * records
 	if (page != pages) {
 		state.trimEnd = state.trimStart + records - 1
@@ -145,12 +96,13 @@ async function pagination(recordCount: number, page: number, records: number) {
 	// Fetch only records that must be displayed on table
 	let recordsLink = "/records?from=" + state.trimStart + "&to=" + state.trimEnd
 
+	state.countRec = 0
 	addRows(recordsLink)
 
 
-	// User info: Display number of records and pages on window
-	let pageDet = el('page-details')
-	pageDet!.innerHTML = `<p>There are <strong>${recordCount + 1}</strong> records and <strong>${pages}</strong> pages</p>`
+	// User info: Display current page number
+	let pageNum = el('page-number')
+	pageNum!.innerHTML = `<p>Page <strong>${state.page}</strong> of <strong>${pages}</strong> </p>`
 
 	return {
 		'pages': pages
@@ -159,9 +111,6 @@ async function pagination(recordCount: number, page: number, records: number) {
 
 // Create paging buttons functions
 function pageButtons(pages: number) {
-	// User info: Display current page number
-	let pageNum = el('page-number')
-	pageNum!.innerHTML = `<p>Page ${state.page}</p>`
 
 	// Select element to create pagination buttons in
 	let wrapper = el('pagination-wrapper')
@@ -224,8 +173,6 @@ async function loadIntoTable(table: any) {
 	let cnumRecords = RECORDCOUNT - 1
 	let hearders = HEADERS
 
-
-
 	// Clear the table
 	tableHead.innerHTML = "<tr></tr>"
 	tableBody.innerHTML = ""
@@ -238,8 +185,10 @@ async function loadIntoTable(table: any) {
 		tableHead.querySelector("tr").appendChild(headerElement)
 	}
 
+	// Add rows in pagination function
 	let rows = pagination(cnumRecords, state.page, state.records)
 
+	// Create pagination buttons
 	pageButtons((await rows).pages)
 
 	// Display content
@@ -247,6 +196,84 @@ async function loadIntoTable(table: any) {
 	$(".content").fadeIn(500);
 }
 
+// Add/remove rows from table based on resize event of the window
+window.addEventListener('resize', function () {
+	// Calculate number rows to be added/deleted
+	let newHeight = Math.floor((window.innerHeight - 160) / 40)
+	let diff = newHeight - state.records
+
+	let start = state.trimEnd + 1
+	let end = state.trimEnd + diff
+	let pages = Math.ceil(RECORDCOUNT / state.records)
+
+
+	if (diff < 0 && state.countRec - 1 == newHeight && state.countRec != 1) {
+		// Delete rows from last page
+		deleteRows(state.countRec, diff)
+
+		state.countRec = state.countRec + diff
+		state.trimEnd = state.trimEnd + diff
+		state.records = newHeight
+		state.page = Math.floor(state.trimEnd / (state.records - 1))
+
+		pages = Math.ceil(RECORDCOUNT / state.records)
+		pageButtons(pages)
+	}
+	else if (diff < 0 && state.page != pages && state.countRec - 1 >= newHeight) {
+		// Delete rows that are not on the last page
+		deleteRows(state.countRec, diff)
+
+		state.countRec = state.countRec + diff
+		state.trimEnd = state.trimEnd + diff
+		state.records = newHeight
+		state.page = Math.floor(state.trimEnd / (state.records - 1))
+
+		pages = Math.ceil(RECORDCOUNT / state.records)
+		pageButtons(pages)
+	}
+	else if (diff > 0 && start <= RECORDCOUNT - 1) {
+		// Add rows if end of data is not yet reached
+		let recordsLink = "/records?from=" + start + "&to=" + end
+		addRows(recordsLink)
+
+		state.trimEnd = state.trimEnd + diff
+		state.records = newHeight
+		state.page = Math.floor(state.trimEnd / (state.records - 1))
+
+		pages = Math.ceil(RECORDCOUNT / state.records)
+		pageButtons(pages)
+	}
+	else {
+		state.records = newHeight
+		let pages = Math.ceil(RECORDCOUNT / state.records)
+		pageButtons(pages)
+	}
+
+})
+
+
+
 window.onload = () => {
 	getData()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//  o   o
+//               /^^^^^7
+// '  '     ,oO))))))))Oo,
+//        ,'))))))))))))))), /{
+//   '  ,'o  ))))))))))))))))={
+//      >    ))))))))))))))))={
+//      `,   ))))))\ \)))))))={
+//        ',))))))))\/)))))' \{
+//          '*O))))))))O*'
