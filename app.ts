7991e74ed0b10a6aec38ca367cc8a57a8b73149f
Koumori97 <d.e.fledermaus86@gmail.com>
2022-04-14 15:16:40 +0200

    next and prev buttons fixed

--- app.ts
+++ app.ts
@@ -1,18 +1,22 @@
-let paramOne: any = 0
-let paramTwo: any = 9
-let contentNeeded: any = [];
-let contentListNeeded: any = []
 const headings: any = document.querySelector("#Headings");
 const content_cols: any = document.querySelector("#Content");
 const lists: any = document.querySelector('#info-id')
 const pageStats: any = document.getElementById('pageStats')
 const clear = "";
-const prevButton: any = document.querySelector('#prev')
-const nextButton: any = document.querySelector('#next')
-let nextClickCount = 0;
-let prevClickCount = 0;
-let nextClicked: boolean
-let prevClicked: boolean
+const nextButton: any = document.querySelector("#next");
+const prevButton: any = document.querySelector("#prev");
+let paramOne: any = 0
+let paramTwo: any = 9
+let contentNeeded: any = [];
+let contentListNeeded: any = []
+let nextCounter = 0;
+let nextBtnStatus: any = false;
+let nextTurnFalse: any;
+let nextActCheck: any;
+let prevCounter = 0;
+let prevBtnStatus: any = false;
+let prevTurnFalse: any;
+let prevActCheck: any;
 
 // Fetch requests
 
@@ -47,45 +51,17 @@ function getTable() {
         for (let i = 0; i < contentList.length; i++) {
             contentNeeded.push(contentList[i])
             cols(contentList[i])
-            // console.log(contentList[i]);
             let contentListContent: any = contentList[i]
             for (let j = 0; j < contentListContent.length; j++) {
-                console.log(contentListContent[j]);
-                contentListNeeded.push(contentListContent[j]);
-                // colContent(contentListContent[j])
+                let rowContentList: any = contentListContent[j]
+                console.log(rowContentList);
             }
+
         }
     }); 
 }
 
-// Targets heading div to creating the 1st row(Column names)
-
-function colHeading(heading: string) {
-    if (heading == "ID") {
-        let headingCol = `<div id="col_heading" class="col_heading">${heading}<input placeholder="Press Enter to search" id="idInput"></div>`;
-        headings.innerHTML += headingCol;
-    }
-    else {
-        let headingCol = `<div id="col_heading" class="col_heading">${heading}</div>`;
-        headings.innerHTML += headingCol;
-    }
-}
-
-function colContent(list: string) {
-    let contentCol = `<div id="col_heading" class="col_heading">${list}</div>`;
-    lists.innerHTML += contentCol
-}
-
-// Targets content div to create the actual table and fill with data
-
-function cols(content: string) {
-    let content_col = `<div id="content_col" class="content_col">
-                        <div class="info" id="info_id">${content}</div>
-                        </div>`;
-    content_cols.innerHTML += content_col
-}
-
-// Display the current shown results
+//// Display the current shown results
 
 function stats() {
     fetch("http://localhost:2050/recordCount", {
@@ -101,7 +77,8 @@ function stats() {
     })
 }
 
-// Load all intial fields/data
+
+// Loads all intial fields/data
 
 window.onload = function() {
     stats()
@@ -109,123 +86,28 @@ window.onload = function() {
     getTable()
 }
 
+// Displaying data into html
 
-// Nav Buttons
+//// Targets heading div to creating the 1st row(Column names)
 
-//// Next Function
-
-function next() {
-    if (paramTwo < 999999) {
-        disableNext()
-
-        let intOne = parseInt(paramOne) + 10
-        let intTwo = parseInt(paramTwo) + 10
-        paramOne = intOne.toString()
-        paramTwo = intTwo.toString()
-
-        stats()
-        clearTable()
-        fetch("http://localhost:2050/records?from=" + paramOne + "&to=" + paramTwo, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        })
-        .then((res) => res.text())
-        .then((data) => {
-            data = JSON.parse(data);
-            let contentList = data;
-            for (let i = 0; i < contentList.length; i++) {
-                contentNeeded.push(contentList[i])
-                cols(contentList[i])
-            }
-        });
-        setTimeout(enableNext, 500)
-    } else {
-        disableNext()
-        paramOne = 0
-        paramTwo = 9
-        stats()
-        clearTable()
-        fetch("http://localhost:2050/records?from=" + paramOne + "&to=" + paramTwo, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        })
-        .then((res) => res.text())
-        .then((data) => {
-            data = JSON.parse(data);
-            let contentList = data;
-            for (let i = 0; i < contentList.length; i++) {
-                contentNeeded.push(contentList[i])
-                cols(contentList[i])
-            }
-        });
-        setTimeout(enableNext, 500)
+function colHeading(heading: string) {
+    if (heading == "ID") {
+        let headingCol = `<div id="col_heading" class="col_heading">${heading}<input placeholder="Press Enter to search" id="idInput"></div>`;
+        headings.innerHTML += headingCol;
+    }
+    else {
+        let headingCol = `<div id="col_heading" class="col_heading">${heading}</div>`;
+        headings.innerHTML += headingCol;
     }
 }
 
-//// Prev Function
+//// Targets content div to create the actual table and fill with data
 
-function prev() {
-    if (paramOne >= 10) {
-        disablePrev()
-
-        let intOne = parseInt(paramOne) - 10
-        let intTwo = parseInt(paramTwo) - 10
-        paramOne = intOne.toString()
-        paramTwo = intTwo.toString()
-        
-        stats()
-        clearTable()
-        fetch("http://localhost:2050/records?from=" + paramOne + "&to=" + paramTwo, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        })
-        .then((res) => res.text())
-        .then((data) => {
-            data = JSON.parse(data);
-            let contentList = data;
-            for (let i = 0; i < contentList.length; i++) {
-                contentNeeded.push(contentList[i])
-                cols(contentList[i])
-            }
-        });
-        setTimeout(enablePrev, 500)
-    } else {
-        disablePrev()
-        paramOne = 999990
-        paramTwo = 999999
-        stats()
-        clearTable()
-        fetch("http://localhost:2050/records?from=" + paramOne + "&to=" + paramTwo, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        })
-        .then((res) => res.text())
-        .then((data) => {
-            data = JSON.parse(data);
-            let contentList = data;
-            for (let i = 0; i < contentList.length; i++) {
-                contentNeeded.push(contentList[i])
-                cols(contentList[i])
-            }
-        });
-        setTimeout(enablePrev, 500)
-    }
-}
-
-// To disable button
-function disablePrev() {
-    prevButton.disabled = true;
-}
-function disableNext() {
-    nextButton.disabled = true;
-}
-
-// To Enable Buttons
-function enablePrev() {
-    prevButton.disabled = false;
-}
-function enableNext() {
-    nextButton.disabled = false;
+function cols(content: string) {
+    let content_col = `<div id="content_col" class="content_col">
+                        <div class="info" id="info_id">${content}</div>
+                        </div>`;
+    content_cols.innerHTML += content_col
 }
 
 // Clear Table (content div)
@@ -268,79 +150,7 @@ window.addEventListener("keydown", (e) => {
     }
 })
 
-// Jump to ID
-
-// window.addEventListener("keydown", (e) => {
-//     let key = e.key;
-//     if (key === "Alt") {
-//         let search: any = document.querySelector('#idJump');
-//         let searchOne = search.value
-//         let convert = parseInt(searchOne) + 9
-//         let searchTwo = convert.toString()
-
-//         if (searchOne != "" && searchOne <= 999990) {
-//             clearTable()
-//             fetch("http://localhost:2050/records?from=" + searchOne + "&to=" + searchTwo, {
-//                 method: "GET",
-//                 headers: { "Content-Type": "application/json" },
-//             })
-//             .then((res) => res.text())
-//             .then((data) => {
-//                 data = JSON.parse(data);
-//                 let contentList = data;
-//                 // console.log(contentList);
-//                 for (let i = 0; i < contentList.length; i++) {
-//                     contentNeeded.push(contentList[i])
-//                     cols(contentList[i])
-//                 }
-//             });
-//             fetch("http://localhost:2050/recordCount", {
-//             method: "GET",
-//             headers: { "Content-Type": "application/json" },
-//             })
-//             .then((res) => res.text())
-//             .then((data) => {
-//                 data = JSON.parse(data);
-//                 let count = data;
-//                 let currentStats = "Showing results from ID " + searchOne + " to " + searchTwo + " out of " + count + " results.";
-//                 pageStats.innerHTML = currentStats;
-//             })
-//         } else if (searchOne > 999990 && searchOne < 1000000) {
-//             clearTable()
-//             fetch("http://localhost:2050/records?from=" + searchOne + "&to=" + 999999, {
-//                 method: "GET",
-//                 headers: { "Content-Type": "application/json" },
-//             })
-//             .then((res) => res.text())
-//             .then((data) => {
-//                 data = JSON.parse(data);
-//                 let contentList = data;
-//                 // console.log(contentList);
-//                 for (let i = 0; i < contentList.length; i++) {
-//                     contentNeeded.push(contentList[i])
-//                     cols(contentList[i])
-//                 }
-//             });
-//             fetch("http://localhost:2050/recordCount", {
-//             method: "GET",
-//             headers: { "Content-Type": "application/json" },
-//             })
-//             .then((res) => res.text())
-//             .then((data) => {
-//                 data = JSON.parse(data);
-//                 let count = data;
-//                 let currentStats = "Showing results from ID " + searchOne + " to " + 999999 + " out of " + count + " results.";
-//                 pageStats.innerHTML = currentStats;
-//             })
-//         } else {
-//             alert("There are no records with that ID!")
-//             clearTable()
-//             stats()
-//             getTable()
-//         }
-//         search.value = clear
-//     }
-// })
+// Jump to ID  
 
 function idJump() {
     let search: any = document.querySelector('#idJump');
@@ -403,19 +213,140 @@ function idJump() {
     search.value = clear
 }
 
-// nextButton.addEventListener("click", function() {
-//     let current = new Date()
-//     let firstHour = current.getHours();
-//     let firstMinute = current.getMinutes();
-//     let firstSecond = current.getSeconds();
-//     console.log("test");
-    
+// Nav Buttons
 
-//     if (firstSecond > 57) {
-//         let currentSecs = current.getSeconds()
-//         if (currentSecs <= firstSecond + 3 ) {
-//             console.log("Works");
-//         }
-//     } 
-// })
+//// Next Function
 
+function next() {
+    if (paramTwo < 999999) {
+
+        let intOne = parseInt(paramOne) + (10 * nextCounter)
+        let intTwo = parseInt(paramTwo) + (10 * nextCounter)
+        paramOne = intOne.toString()
+        paramTwo = intTwo.toString()
+
+        stats()
+        clearTable()
+        fetch("http://localhost:2050/records?from=" + paramOne + "&to=" + paramTwo, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        })
+        .then((res) => res.text())
+        .then((data) => {
+            data = JSON.parse(data);
+            let contentList = data;
+            for (let i = 0; i < contentList.length; i++) {
+                contentNeeded.push(contentList[i])
+                cols(contentList[i])
+            }
+        });
+    } else {
+        paramOne = 0
+        paramTwo = 9
+        stats()
+        clearTable()
+        fetch("http://localhost:2050/records?from=" + paramOne + "&to=" + paramTwo, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        })
+        .then((res) => res.text())
+        .then((data) => {
+            data = JSON.parse(data);
+            let contentList = data;
+            for (let i = 0; i < contentList.length; i++) {
+                contentNeeded.push(contentList[i])
+                cols(contentList[i])
+            }
+        });
+    }
+    nextCounter = 0
+}
+
+nextButton.addEventListener("click", () => {
+    clearTimeout(nextTurnFalse)
+    clearTimeout(nextBtnStatus)
+    nextCounter += 1
+    nextBtnStatus = true
+    nextTurnFalse = setTimeout(nextBtnFalse, 2000)
+    nextActCheck = setTimeout(nextCheck, 2100)
+})
+
+function nextBtnFalse() {
+    nextBtnStatus = false
+}
+
+function nextCheck() {
+    if (nextBtnStatus) {
+        // pass
+    } else {
+        next()
+    }
+}
+
+//// Prev Function
+
+function prev() {
+    if (paramOne >= 10) {
+
+        let intOne = parseInt(paramOne) - (10 * prevCounter)
+        let intTwo = parseInt(paramTwo) - (10 * prevCounter)
+        paramOne = intOne.toString()
+        paramTwo = intTwo.toString()
+        
+        stats()
+        clearTable()
+        fetch("http://localhost:2050/records?from=" + paramOne + "&to=" + paramTwo, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        })
+        .then((res) => res.text())
+        .then((data) => {
+            data = JSON.parse(data);
+            let contentList = data;
+            for (let i = 0; i < contentList.length; i++) {
+                contentNeeded.push(contentList[i])
+                cols(contentList[i])
+            }
+        });
+    } else {
+        paramOne = 999990
+        paramTwo = 999999
+        stats()
+        clearTable()
+        fetch("http://localhost:2050/records?from=" + paramOne + "&to=" + paramTwo, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        })
+        .then((res) => res.text())
+        .then((data) => {
+            data = JSON.parse(data);
+            let contentList = data;
+            for (let i = 0; i < contentList.length; i++) {
+                contentNeeded.push(contentList[i])
+                cols(contentList[i])
+            }
+        });
+    }
+    prevCounter = 0
+}
+
+prevButton.addEventListener("click", () => {
+    clearTimeout(prevTurnFalse)
+    clearTimeout(prevBtnStatus)
+    prevCounter += 1
+    prevBtnStatus = true
+    prevTurnFalse = setTimeout(prevBtnFalse, 2000)
+    prevActCheck = setTimeout(prevCheck, 2100)
+})
+
+function prevBtnFalse() {
+    prevBtnStatus = false
+}
+
+function prevCheck() {
+    if (prevBtnStatus) {
+        // pass
+    } else {
+        prev()
+    }
+}
--- server/main.go
+++ server/main.go
@@ -14,10 +14,10 @@ import (
 )
 
 const recordCount = 1000000
-const columnCount = 12
+const columnCount = 11
 const delayResponse = 500 * time.Millisecond
 
-var columns = [columnCount]string{"ID", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"}
+var columns = [columnCount]string{"ID", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func main() {
