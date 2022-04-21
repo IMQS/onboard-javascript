6c53b8d57d044755eac32408e2bc9756510e9646
Koumori97 <d.e.fledermaus86@gmail.com>
2022-04-21 15:52:57 +0200

    window resizing

--- app.ts
+++ app.ts
@@ -6,10 +6,9 @@ const clear = "";
 const nextButton: any = document.querySelector("#next");
 const prevButton: any = document.querySelector("#prev");
 let arrChild: any = []
-let paramOne: any = 0
-let paramTwo: any = 9
+let paramOne: any = "0"
+let paramTwo: any = "9"
 let contentNeeded: any = [];
-let contentListNeeded: any = []
 let nextCounter = 0;
 let nextBtnStatus: any = false;
 let nextTurnFalse: any;
@@ -49,7 +48,6 @@ function getTable() {
     .then((data) => {
         data = JSON.parse(data);
         let contentList = data;
-        // console.log(contentList);
         for (let i = 0; i < contentList.length; i++) {
             let contentListContent: any = contentList[i]
             cols(contentListContent)
@@ -77,9 +75,41 @@ function stats() {
 // Loads all intial fields/data
 
 window.onload = function() {
-    stats()
-    getColumns()
-    getTable()
+    let windowHeight = window.innerHeight;
+
+    if (windowHeight < 600 && windowHeight >= 480) {
+        paramOne = paramOne
+        paramTwo = parseInt(paramOne) + 6
+        paramTwo = paramTwo.toString()
+        stats()
+        getColumns()
+        getTable()
+    } else if (windowHeight < 480 && windowHeight >= 400) {
+        paramOne = paramOne
+        paramTwo = parseInt(paramOne) + 4
+        paramTwo = paramTwo.toString()
+        stats()
+        getColumns()
+        getTable()
+    } else if (windowHeight < 400 && windowHeight > 300) {
+        paramOne = paramOne
+        paramTwo = parseInt(paramOne) + 2
+        paramTwo = paramTwo.toString()
+        stats()
+        getColumns()
+        getTable()
+    } else if (windowHeight <= 300) {
+        paramOne = paramOne
+        paramTwo = parseInt(paramOne) + 1
+        paramTwo = paramTwo.toString()
+        stats()
+        getColumns()
+        getTable()
+    } else {
+        stats()
+        getColumns()
+        getTable()
+    }
 }
 
 // Displaying data into html
@@ -108,54 +138,37 @@ function cols(content: any) {
     }
 }
 
-
-
 // Clear Table (content div)
 
 function clearTable() {
     content_cols.innerHTML = clear
 }
 
-// // ID Search
-
-// window.addEventListener("keydown", (e) => {
-//     let key = e.key;
-
-//     if (key === "Enter") {
-//         let search: any = document.querySelector('#idInput');
-//         clearTable()
-//         if (search.value != "" && search.value < 1000000 && search.value >= 0) {
-//             pageStats.innerHTML = clear
-//             let searchStats = "Showing result with ID of " + search.value;
-//             pageStats.innerHTML = searchStats;
-//             fetch("http://localhost:2050/records?from=" + search.value + "&to=" + search.value, {
-//                 method: "GET",
-//                 headers: { "Content-Type": "application/json" },
-//             })
-//             .then((res) => res.text())
-//             .then((data) => {
-//                 data = JSON.parse(data);
-//                 let contentList = data;
-//                 for (let i = 0; i < contentList.length; i++) {
-//                     contentNeeded.push(contentList[i])
-//                     cols(contentList[i])
-//                 }
-//             });
-//         } else {
-//             alert("There are no results with that ID!")
-//             stats()
-//             getTable()
-//         }
-//         search.value = clear
-//     }
-// })
-
 // Jump to ID  
 
 function idJump() {
+    let windowHeight = window.innerHeight;
+    let toConvert: any;
+
+    if (windowHeight < 600 && windowHeight >= 480) {
+        toConvert = 6
+    }
+    else if (windowHeight < 480 && windowHeight >= 400) {
+        toConvert = 4
+    }
+    else if (windowHeight < 400 && windowHeight > 300) {
+        toConvert = 2
+    }
+    else if (windowHeight <= 300) {
+        toConvert = 1
+    }
+    else {
+        toConvert = 9
+    }
+
     let search: any = document.querySelector('#idJump');
     let searchOne = search.value
-    let convert = parseInt(searchOne) + 9
+    let convert = parseInt(searchOne) + toConvert
     let searchTwo = convert.toString()
     paramOne = searchOne
     paramTwo = searchTwo
@@ -176,7 +189,8 @@ function idJump() {
             }
         });
         stats()
-    } else if ( searchOne > 999990 && searchOne < 1000000 && searchOne >= 0 && searchOne !== 'e') {
+    } 
+    else if ( searchOne > 999990 && searchOne < 1000000 && searchOne >= 0 && searchOne !== 'e') {
         clearTable()
         fetch("http://localhost:2050/records?from=" + searchOne + "&to=" + 999999, {
             method: "GET",
@@ -202,7 +216,8 @@ function idJump() {
             let currentStats = "Showing results from ID " + searchOne + " to " + 999999 + " out of " + count + " results.";
             pageStats.innerHTML = currentStats;
         })
-    } else {
+    }
+     else {
         alert("There are no records with that ID! Please check that your input does not exceed 999 999 and is not a negative amount.")
         clearTable()
         paramOne = 0
@@ -218,46 +233,19 @@ function idJump() {
 //// Next Function
 
 function next() {
-    if (paramTwo < 999999) {
+    if (paramTwo == 999999) {
+        paramOne = "0"
 
-        let intOne = parseInt(paramOne) + (10 * nextCounter)
-        let intTwo = parseInt(paramTwo) + (10 * nextCounter)
+        resizing()
+    } else {
+        let nextAmount: any = (paramTwo - paramOne) + 1
+
+        let intOne = parseInt(paramOne) + (nextAmount * nextCounter)
+        let intTwo = parseInt(paramTwo) + (nextAmount * nextCounter)
         paramOne = intOne.toString()
         paramTwo = intTwo.toString()
 
-        stats()
-        clearTable()
-        fetch("http://localhost:2050/records?from=" + paramOne + "&to=" + paramTwo, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        })
-        .then((res) => res.text())
-        .then((data) => {
-            data = JSON.parse(data);
-            let contentList = data;
-            for (let i = 0; i < contentList.length; i++) {
-                contentNeeded.push(contentList[i])
-                cols(contentList[i])
-            }
-        });
-    } else {
-        paramOne = 0
-        paramTwo = 9
-        stats()
-        clearTable()
-        fetch("http://localhost:2050/records?from=" + paramOne + "&to=" + paramTwo, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        })
-        .then((res) => res.text())
-        .then((data) => {
-            data = JSON.parse(data);
-            let contentList = data;
-            for (let i = 0; i < contentList.length; i++) {
-                contentNeeded.push(contentList[i])
-                cols(contentList[i])
-            }
-        });
+        resizing()
     }
     nextCounter = 0
 }
@@ -286,46 +274,19 @@ function nextCheck() {
 //// Prev Function
 
 function prev() {
-    if (paramOne >= 10) {
+    if (paramOne == 0) {
+        paramTwo = "999999"
+        
+        prevRe()
+    } else {
+        let prevAmount: any = (paramTwo - paramOne) + 1
 
-        let intOne = parseInt(paramOne) - (10 * prevCounter)
-        let intTwo = parseInt(paramTwo) - (10 * prevCounter)
+        let intOne = parseInt(paramOne) - (prevAmount * prevCounter)
+        let intTwo = parseInt(paramTwo) - (prevAmount * prevCounter)
         paramOne = intOne.toString()
         paramTwo = intTwo.toString()
-        
-        stats()
-        clearTable()
-        fetch("http://localhost:2050/records?from=" + paramOne + "&to=" + paramTwo, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        })
-        .then((res) => res.text())
-        .then((data) => {
-            data = JSON.parse(data);
-            let contentList = data;
-            for (let i = 0; i < contentList.length; i++) {
-                contentNeeded.push(contentList[i])
-                cols(contentList[i])
-            }
-        });
-    } else {
-        paramOne = 999990
-        paramTwo = 999999
-        stats()
-        clearTable()
-        fetch("http://localhost:2050/records?from=" + paramOne + "&to=" + paramTwo, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        })
-        .then((res) => res.text())
-        .then((data) => {
-            data = JSON.parse(data);
-            let contentList = data;
-            for (let i = 0; i < contentList.length; i++) {
-                contentNeeded.push(contentList[i])
-                cols(contentList[i])
-            }
-        });
+
+        prevRe()
     }
     prevCounter = 0
 }
@@ -348,5 +309,147 @@ function prevCheck() {
         // pass
     } else {
         prev()
+    }
+}
+
+
+
+
+function resizing() {
+    let windowHeight = window.innerHeight;
+
+    if (windowHeight < 600 && windowHeight >= 480) {
+        if (paramOne > 999993) {
+            paramOne = 999993
+        } else {
+            //pass
+        }
+        paramTwo = parseInt(paramOne) + 6
+        paramTwo = paramTwo.toString()
+        clearTable()
+        stats()
+        getTable()
+    } 
+    else if (windowHeight < 480 && windowHeight >= 400) {
+        if (paramOne > 999995) {
+            paramOne = 999995
+        } else {
+            //pass
+        }
+        paramTwo = parseInt(paramOne) + 4
+        paramTwo = paramTwo.toString()
+        clearTable()
+        stats()
+        getTable()
+    } 
+    else if (windowHeight < 400 && windowHeight > 300) {
+        if (paramOne > 999997) {
+            paramOne = 999997
+        } else {
+            //pass
+        }
+        paramTwo = parseInt(paramOne) + 2
+        paramTwo = paramTwo.toString()
+        clearTable()
+        stats()
+        getTable()
+    } 
+    else if (windowHeight <= 300) {
+        if (paramOne > 999998) {
+            paramOne = 999998
+        } else {
+            //pass
+        }
+        paramTwo = parseInt(paramOne) + 1
+        paramTwo = paramTwo.toString()
+        clearTable()
+        stats()
+        getTable()
+    }
+    else {
+        if (paramOne > 999990) {
+            paramOne = 999990
+        } else {
+            //pass
+        }
+        paramTwo = parseInt(paramOne) + 9
+        paramTwo = paramTwo.toString()
+        clearTable()
+        stats()
+        getTable()
+    }
+}
+
+let runit: any;
+
+window.addEventListener("resize", () => {
+    clearTimeout(runit);
+    runit = setTimeout(resizing, 100)
+    
+});
+
+
+function prevRe() {
+    let windowHeight = window.innerHeight;
+
+    if (windowHeight < 600 && windowHeight >= 480) {
+        if (paramOne == 0) {
+            paramOne = 999993
+        } else {
+            //pass
+        }
+        paramTwo = parseInt(paramOne) + 6
+        paramTwo = paramTwo.toString()
+        clearTable()
+        stats()
+        getTable()
+    } 
+    else if (windowHeight < 480 && windowHeight >= 400) {
+        if (paramOne == 0) {
+            paramOne = 999995
+        } else {
+            //pass
+        }
+        paramTwo = parseInt(paramOne) + 4
+        paramTwo = paramTwo.toString()
+        clearTable()
+        stats()
+        getTable()
+    } 
+    else if (windowHeight < 400 && windowHeight > 300) {
+        if (paramOne == 0) {
+            paramOne = 999997
+        } else {
+            //pass
+        }
+        paramTwo = parseInt(paramOne) + 2
+        paramTwo = paramTwo.toString()
+        clearTable()
+        stats()
+        getTable()
+    } 
+    else if (windowHeight <= 300) {
+        if (paramOne == 0) {
+            paramOne = 999999
+        } else {
+            //pass
+        }
+        paramTwo = parseInt(paramOne) + 1
+        paramTwo = paramTwo.toString()
+        clearTable()
+        stats()
+        getTable()
+    }
+    else {
+        if (paramOne == 0) {
+            paramOne = 999990
+        } else {
+            //pass
+        }
+        paramTwo = parseInt(paramOne) + 9
+        paramTwo = paramTwo.toString()
+        clearTable()
+        stats()
+        getTable()
     }
 }
