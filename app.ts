9c05769d4798fa7ce36c635b3f85859361852211
AshtonMar <ashtonmartingoliath@gmail.com>
2022-05-09 14:51:43 +0200

    Latest Changes

--- app.ts
+++ app.ts
@@ -4,7 +4,11 @@ let count = 0;
 let timeout = 0;
 
 window.onload = () => {
+    createNavigation();
+    recordCount();
+    headingRowCreation();
     fromNumber = 0;
+    new pageNavigation();
 };
 
 function checkResponseError(response: Response) {
@@ -14,7 +18,7 @@ function checkResponseError(response: Response) {
     return response;
 }
 
-let debounce = (func: any, delay: number) => {
+function debounce(func: any, delay: number) {
     return function () {
         clearTimeout(timeout);
 
@@ -22,45 +26,48 @@ let debounce = (func: any, delay: number) => {
             func();
         }, delay);
     };
-};
+}
 
 function createNavigation() {
     let recordNav: HTMLElement | null = document.getElementById("record-navigation-container"); // Navigation area
     if (recordNav !== null) {
         recordNav.innerHTML = `
-  <div id="navigation-btns">
-    <button value="first" id="first-page-btn">First Page</button>  
-    <button value="previous" id="previous-records-btn">Previous</button>
-    <button value="next" id="next-records-btn">Next</button>
-    <button value="last" id="last-page-btn">Last Page</button>  
-    <button id="confirmation-btn">Get Record</button>
-  </div>
-  <div class="current-page-container">
-    <p id=current-page></p>
-  </div>
-  `;
+        <div id="navigation-btns">
+            <button value="first" id="first-page-btn">First Page</button>
+            <button value="previous" id="previous-records-btn">Previous</button>
+            <button value="next" id="next-records-btn">Next</button>
+            <button value="last" id="last-page-btn">Last Page</button>
+            <button id="confirmation-btn">Get Record</button>
+        </div>
+        <div class="current-page-container">
+            <p id="current-page"></p>
+        </div>
+        `;
     }
 }
 
-function getRecords(fromNumber: number, toNumber: number) {
+function getRecords(fromNumber: number, toNumber: number): Promise<void> {
     return fetch(`http://localhost:2050/records?from=${fromNumber}&to=${toNumber}`, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
     })
         .then(checkResponseError)
-        .then((response: Response) => response.text())
-        .then((data) => {
-            let columnDataList = JSON.parse(data);
+        .then((response: Response) => response.json())
+        .then((data: string) => {
             let infoColumns: HTMLElement | null = document.getElementById("info-columns-container"); // Information
+            let currentPage: HTMLElement | null = document.getElementById("current-page");
 
-            console.log("Hello");
-            (infoColumns as HTMLDivElement).innerHTML = "";
-            for (let i = 0; i < columnDataList.length; i++) {
-                dynamicGrid(columnDataList[i]);
+            if (infoColumns !== null) {
+                (infoColumns as HTMLDivElement).innerHTML = "";
+
+                for (let i of data) {
+                    dynamicGrid(i);
+                }
             }
 
-            let currentPage: HTMLElement | null = document.getElementById("current-page");
-            (currentPage as HTMLParagraphElement).innerHTML = `${fromNumber}  / ${toNumber}.`;
+            if (currentPage !== null) {
+                (currentPage as HTMLParagraphElement).innerHTML = `${fromNumber}  / ${toNumber}.`;
+            }
         })
         .catch((error: Error) => {
             console.log(error);
@@ -69,26 +76,19 @@ function getRecords(fromNumber: number, toNumber: number) {
 
 function recordSelection() {
     let recordNav: HTMLElement | null = document.getElementById("record-navigation-container"); // Navigation area
-    if (recordNav !== null) {
+    if (!recordNav) {
+        alert("The navigation is not working correctly refresh the page");
+    } else {
         let singleRecordSelection = `
-  <button id="return-btn">Return</button>
-    <div id="user-input-data">
-      <div class="navigation-input-area-id" id="id">
-        <label class="record-labels" for="record-id"
-          >Enter record ID :
-        </label>
-        <input
-          type="text"
-          name="record-id"
-          id="record-id"
-          class="navigation-input"
-          value="0"
-        />
-      </div>
-      <p class="amount-of-records"></p>
-    </div>
-  <button id="get-record-btn">See Record</button>
-  `;
+        <button id="return-btn">Return</button>
+        <div id="user-input-data">
+            <div class="navigation-input-area-id" id="id">
+                <label class="record-labels" for="record-id">Enter record ID :</label>
+                <input type="text" name="record-id" id="record-id" class="navigation-input" value="0" />
+            </div>
+            <p class="amount-of-records"></p>
+        </div>
+        <button id="get-record-btn">See Record</button>`;
 
         recordNav.innerHTML = singleRecordSelection;
 
@@ -97,76 +97,75 @@ function recordSelection() {
         let numberOfRows = Math.floor(window.innerHeight / 50);
         let getSingleRecord: HTMLElement | null = document.getElementById("get-record-btn");
 
-        returnBtn?.addEventListener("click", () => {
-            console.log("hello");
-            createNavigation();
-            fromNumber = 0;
-            let toNumber = fromNumber + numberOfRows;
-            getRecords(fromNumber, toNumber);
-        });
+        if (returnBtn !== null) {
+            // Resets to the first page
+            returnBtn.addEventListener("click", () => {
+                createNavigation();
+                fromNumber = 0;
+                getRecords(fromNumber, fromNumber + numberOfRows);
+            });
+        }
 
-        getSingleRecord?.addEventListener("click", () => {
-            let recordIdValue = (recordIdInput as HTMLInputElement).value;
-            fromNumber = Number(recordIdValue);
+        if (getSingleRecord !== null) {
+            getSingleRecord.addEventListener("click", () => {
+                let recordIdValue = (recordIdInput as HTMLInputElement).value;
+                fromNumber = Number(recordIdValue);
 
-            console.log(fromNumber);
+                let toNumber = fromNumber + numberOfRows;
+                let finalRecord = recordNumberTotal - 1;
 
-            let toNumber = fromNumber + numberOfRows;
-            let finalRecord = recordNumberTotal - 1;
+                if (toNumber > finalRecord) {
+                    toNumber = finalRecord;
+                    fromNumber = toNumber - numberOfRows;
+                }
 
-            if (toNumber > finalRecord) {
-                toNumber = finalRecord;
-                fromNumber = toNumber - numberOfRows;
-            }
+                let check = ["undefined", "string", ""];
 
-            let check = ["undefined", "string", ""];
-
-            if (check.includes(typeof fromNumber) || fromNumber < 0) {
-                alert("Does not exists");
-                recordIdValue = "0";
-            } else if (typeof fromNumber === "number" && fromNumber >= 0) {
-                getRecords(fromNumber, toNumber);
-            }
-        });
+                if (check.includes(typeof fromNumber) || fromNumber < 0) {
+                    alert("Does not exists");
+                    recordIdValue = "0";
+                } else if (typeof fromNumber === "number" && fromNumber >= 0) {
+                    getRecords(fromNumber, toNumber);
+                }
+            });
+        }
     }
 }
 
-function createHeadingGrid(headings: any) {
+function createHeadingGrid(headings: string) {
     let headingColumns: HTMLElement | null = document.getElementById("column-headings-container"); // Headings
     let headingsData = `<h1 class="column-heading">${headings}</h1>`;
 
-    (headingColumns as HTMLDivElement).innerHTML += headingsData;
+    if (headingColumns !== null) {
+        (headingColumns as HTMLDivElement).innerHTML += headingsData;
+    }
 }
 
-function recordCount() {
+function recordCount(): Promise<void> {
     return fetch("http://localhost:2050/recordCount", {
         method: "GET",
         headers: { "Content-Type": "application/json" },
     })
         .then(checkResponseError)
-        .then((response: Response) => response.text())
-        .then((data) => {
-            recordNumberTotal = JSON.parse(data);
+        .then((response: Response) => response.json())
+        .then((data: number) => {
+            recordNumberTotal = data;
         })
         .catch((error: Error) => {
             console.log(error);
         });
 }
 
-function headingRowCreation() {
+function headingRowCreation(): Promise<void> {
     return fetch("http://localhost:2050/columns", {
         method: "GET",
         headers: { "Content-Type": "application/json" },
     })
         .then(checkResponseError)
-        .then((response: Response) => response.text())
-        .then((data) => {
-            let headingDataList = JSON.parse(data);
-            let headings: string;
-
-            for (let i = 0; i < headingDataList.length; i++) {
-                headings = headingDataList[i];
-                createHeadingGrid(headings);
+        .then((response: Response) => response.json())
+        .then((data: string) => {
+            for (let i of data) {
+                createHeadingGrid(i);
             }
             resizeScreenData();
         })
@@ -175,7 +174,7 @@ function headingRowCreation() {
         });
 }
 
-function dynamicGrid(columnData: any) {
+function dynamicGrid(columnData: string) {
     let infoColumns: HTMLElement | null = document.getElementById("info-columns-container"); // Information
     // Creates the row that the info will display and adds it to the infoColumnsArea.
     let infoDataRow = `<div id="info-row-${columnData[0]}" class="info-rows"></div>`;
@@ -186,8 +185,8 @@ function dynamicGrid(columnData: any) {
         let finalInfoDataRow = document.getElementById("info-row-" + columnData[0]);
         if (finalInfoDataRow !== null) {
             // Loops through
-            for (let x = 0; x < columnData.length; x++) {
-                let infoData = `<p class="info-row-data">${columnData[x]}</p>`;
+            for (let x of columnData) {
+                let infoData = `<p class="info-row-data">${x}</p>`;
                 finalInfoDataRow.innerHTML += infoData;
             }
         }
@@ -226,87 +225,95 @@ function resizeScreenData() {
     }
 }
 
-recordCount();
-window?.addEventListener("resize", debounce(resizeScreenData, 500));
-createNavigation();
-headingRowCreation();
+window.addEventListener("resize", debounce(resizeScreenData, 500));
 
-{
-    let confirmationBtn: HTMLElement | null = document.getElementById("confirmation-btn");
-    confirmationBtn?.addEventListener("click", recordSelection);
+class pageNavigation {
+    nextBtn: HTMLElement | null;
+    previousBtn: HTMLElement | null;
+    firstPageBtn: HTMLElement | null;
+    lastPageBtn: HTMLElement | null;
+    confirmationBtn: HTMLElement | null;
 
-    let nextBtn: HTMLElement | null = document.getElementById("next-records-btn");
-    let previousBtn: HTMLElement | null = document.getElementById("previous-records-btn");
-    let firstPageBtn: HTMLElement | null = document.getElementById("first-page-btn");
-    let lastPageBtn: HTMLElement | null = document.getElementById("last-page-btn");
+    constructor() {
+        this.nextBtn = document.getElementById("next-records-btn");
+        this.previousBtn = document.getElementById("previous-records-btn");
+        this.firstPageBtn = document.getElementById("first-page-btn");
+        this.lastPageBtn = document.getElementById("last-page-btn");
+        this.confirmationBtn = document.getElementById("confirmation-btn");
 
-    let nextPage = () => {
-        console.log(fromNumber);
-        let numberOfRows = Math.floor(window.innerHeight / 50);
-        fromNumber = fromNumber + numberOfRows * count;
-        let toNumber = fromNumber + numberOfRows;
-
-        let finalRecord = recordNumberTotal - 1;
-
-        (previousBtn as HTMLButtonElement).disabled = false;
-
-        if (toNumber >= finalRecord) {
-            (nextBtn as HTMLButtonElement).disabled = true;
-            fromNumber = finalRecord - numberOfRows;
+        if (this.confirmationBtn !== null) {
+            this.confirmationBtn.addEventListener("click", recordSelection);
         }
 
-        getRecords(fromNumber, toNumber);
-        count = 0;
-    };
-    nextBtn?.addEventListener("click", () => {
-        count++;
-        nextPage = debounce(nextPage, 500);
-        nextPage();
-    });
+        if (this.nextBtn !== null && this.previousBtn !== null && this.firstPageBtn !== null && this.lastPageBtn !== null) {
+            let nextPage = () => {
+                let numberOfRows = Math.floor(window.innerHeight / 50);
+                fromNumber = fromNumber + numberOfRows * count;
+                let toNumber = fromNumber + numberOfRows;
 
-    let previousPage = () => {
-        let numberOfRows = Math.floor(window.innerHeight / 50);
-        fromNumber = fromNumber - numberOfRows * count;
-        let toNumber = fromNumber + numberOfRows;
-        (nextBtn as HTMLButtonElement).disabled = false;
+                let finalRecord = recordNumberTotal - 1;
 
-        if (fromNumber <= 0) {
-            (previousBtn as HTMLButtonElement).disabled = true;
-            fromNumber = 0;
+                (this.previousBtn as HTMLButtonElement).disabled = false;
+
+                if (toNumber >= finalRecord) {
+                    (this.nextBtn as HTMLButtonElement).disabled = true;
+                    fromNumber = finalRecord - numberOfRows;
+                }
+
+                getRecords(fromNumber, toNumber);
+                count = 0;
+            };
+            this.nextBtn.addEventListener("click", () => {
+                count++;
+                nextPage = debounce(nextPage, 500);
+                nextPage();
+            });
+
+            let previousPage = () => {
+                let numberOfRows = Math.floor(window.innerHeight / 50);
+                fromNumber = fromNumber - numberOfRows * count;
+                let toNumber = fromNumber + numberOfRows;
+                (this.nextBtn as HTMLButtonElement).disabled = false;
+
+                if (fromNumber <= 0) {
+                    (this.previousBtn as HTMLButtonElement).disabled = true;
+                    fromNumber = 0;
+                }
+
+                getRecords(fromNumber, toNumber);
+                count = 0;
+            };
+            this.previousBtn.addEventListener("click", () => {
+                count++;
+                previousPage = debounce(previousPage, 500);
+                previousPage();
+            });
+
+            let firstPage = () => {
+                fromNumber = 0;
+                let numberOfRows = Math.floor(window.innerHeight / 50);
+                let toNumber = fromNumber + numberOfRows;
+                (this.nextBtn as HTMLButtonElement).disabled = false;
+                (this.previousBtn as HTMLButtonElement).disabled = true;
+
+                getRecords(fromNumber, toNumber);
+            };
+            this.firstPageBtn.addEventListener("click", () => {
+                firstPage();
+            });
+
+            let lastPage = () => {
+                let finalRecord = recordNumberTotal - 1;
+                let numberOfRows = Math.floor(window.innerHeight / 50);
+                fromNumber = finalRecord - numberOfRows;
+                (this.nextBtn as HTMLButtonElement).disabled = true;
+                (this.previousBtn as HTMLButtonElement).disabled = false;
+
+                getRecords(fromNumber, finalRecord);
+            };
+            this.lastPageBtn.addEventListener("click", () => {
+                lastPage();
+            });
         }
-
-        getRecords(fromNumber, toNumber);
-        count = 0;
-    };
-    previousBtn?.addEventListener("click", () => {
-        count++;
-        previousPage = debounce(previousPage, 500);
-        previousPage();
-    });
-
-    let firstPage = () => {
-        fromNumber = 0;
-        let numberOfRows = Math.floor(window.innerHeight / 50);
-        let toNumber = fromNumber + numberOfRows;
-        (nextBtn as HTMLButtonElement).disabled = false;
-        (previousBtn as HTMLButtonElement).disabled = true;
-
-        getRecords(fromNumber, toNumber);
-    };
-    firstPageBtn?.addEventListener("click", () => {
-        firstPage();
-    });
-
-    let lastPage = () => {
-        let finalRecord = recordNumberTotal - 1;
-        let numberOfRows = Math.floor(window.innerHeight / 50);
-        fromNumber = finalRecord - numberOfRows;
-        (nextBtn as HTMLButtonElement).disabled = true;
-        (previousBtn as HTMLButtonElement).disabled = false;
-
-        getRecords(fromNumber, finalRecord);
-    };
-    lastPageBtn?.addEventListener("click", () => {
-        lastPage();
-    });
+    }
 }
