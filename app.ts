a2b438f908c75340540e73977f7e2acffac3ec6a
riaan <kawasakiriaan@gmail.com>
2023-08-28 11:54:12 +0200

    Table Index Values Load in Browser Success Rev 1.1

--- app.ts
+++ app.ts
@@ -1,1 +1,27 @@
-window.onload = () => { $("body").text("Hello world"); }
+window.onload = () => { $("footer").text("Hello Riaan Theron!!"); 
+
+$.ajax({
+    url: '/columns',
+    method: 'GET',
+    success: function(data) {
+      const columnNames = JSON.parse(data);
+      const tableHeader = $('<thead></thead>');
+      //console.log('Success:', data);
+
+      columnNames.forEach(function(columnName: string) {
+        const th = $('<th></th>').text(columnName);
+        tableHeader.append(th);
+      });
+
+      // Append the table header to your table using the correct selector
+      $('#myTable thead').replaceWith(tableHeader);
+    },
+    error: function(xhr, status, error) {
+      console.error('Error fetching column names:', error);
+    }
+  });
+
+  // Fetch and display data, and implement navigation controls in the following steps...
+};
+
+
