eef44e5cd8096dc375f4edb2ef10bd61ad5b8f98
ThokozaniNqwili <simthokozile@gmail.com>
2023-08-29 08:17:00 +0200

    update requested changes

--- app.ts
+++ app.ts
@@ -1,48 +1,59 @@
 import { ajax, css } from "jquery";
-let firstNumber: number = 0;
-let lastNumber: number = 0;
-
+let firstNumber = 0;
+let lastNumber = 0;
+let backend: string = "http://localhost:2050";
 
 async function fetchRecordCount(): Promise<number> {
 	try {
-		const recordCount = await fetch(`http://localhost:2050/recordCount`);
-		if (!recordCount.ok) {
+		const response = await fetch(`${backend}/recordCount`);
+		if (!response.ok) {
 			throw new Error('Failed to fetch record count');
 		}
-		const data = await recordCount.json()
-		return data;
+		return response.json()
 	} catch (error) {
 		console.error('Error fetching the record count:', error);
 		throw error;
-	}
-}
+	};
+};
 
-function fetchColumns(): void {
-	fetch("http://localhost:2050/columns")
-		.then((response: Response) => {
-			return response.json() as Promise<string[]>;
-		})
-		.then((columns: string[]) => {
-			const colArray = columns;
+async function fetchColumns(): Promise<string[]> {
+	try {
+		const response = await fetch(`${backend}/columns`);
+		if (!response.ok) {
+			throw new Error('Failed to fetch columns');
+		}
+		const jsonText = await response.text();
+		const columns: string[] = await JSON.parse(jsonText);
+		return columns
+	} catch (error) {
+		console.error('Error fetching columns', error)
+		throw error
+	};
+};
 
-			for (let c = 0; c < colArray.length; c++) {
-				$(".head").append(`<th>${colArray[c]}</th>`);
-			}
-		})
-}
-fetchColumns()
+async function createTable() {
+	try {
+		const columns = await fetchColumns()
+		for (const col of columns) {
+			$(".head").append(`<th>${col}</th>`);
+		}
+	} catch (error) {
+		console.error('Error creating table', error)
+	};
+};
+createTable()
 
 function adjustRowsByScreenHeight() {
 	const screenHeight = window.innerHeight;
-	const availableHeight = screenHeight - 105 // subtracts the space used from the screeen
-	let rowHeight = 35
+	const availableHeight = screenHeight - 105; // subtracts the space used from the screeen
+	let rowHeight = 35;
 	if (availableHeight <= 0) {
 		return 0;
 	} else {
 		let maxRows = Math.floor(availableHeight / rowHeight);
 		return maxRows;
-	}
-}
+	};
+};
 
 $(window).on('resize', async () => {
 	let resizeTimeout: number;
@@ -55,15 +66,20 @@ $(window).on('resize', async () => {
 		}
 		$('#loader').hide();
 	}, 500);
-})
+});
 
 async function fetchRecords(from: number, to: number): Promise<any[]> {
-	const response = await fetch(`http://localhost:2050/records?from=${from}&to=${to}`);
-	if (!response.ok) {
-		throw new Error("Sorry, there's a problem with the network");
+	try {
+		const response = await fetch(`${backend}/records?from=${from}&to=${to}`);
+		if (!response.ok) {
+			throw new Error("Sorry, there's a problem with the network");
+		}
+		return response.json();
+	} catch (error) {
+		console.error('Error fetching records from server', error);
+		throw error
 	}
-	return response.json();
-}
+};
 
 async function displayRecords(): Promise<void> {
 	try {
@@ -73,8 +89,7 @@ async function displayRecords(): Promise<void> {
 		const inputValue = $('#searchInput').val() as number;
 		if (calculatedRows === 0) {
 			lastNumber = firstNumber
-		}
-		if (firstNumber < 0) {
+		} else if (firstNumber < 0) {
 			firstNumber = 0;
 			lastNumber = firstNumber + (calculatedRows - 1);
 		} else {
@@ -93,24 +108,24 @@ async function displayRecords(): Promise<void> {
 			$('#page').empty();
 			$('#page').append(`Showing record: ${firstNumber} - ${count}`);
 			$('#loader').hide()
-		}
+		};
 		const tbody = $("tbody");
 		tbody.empty();
-		for (let r = 0; r < records.length; r++) {
+		for (const record of records) {
 			$("tbody").append(`<tr class="row"></tr>`); // creates row for each record
 			const lastRow = $(".row:last");
-			for (let i = 0; i < records[r].length; i++) {
-				lastRow.append(`<td>${records[r][i]}</td>`); //assign each record to their column in a specified row
+			for (const value of record) {
+				lastRow.append(`<td>${value}</td>`); // assign each record to their column in a specified row
 			}
-			if (records[r].includes(inputValue)) {
-				lastRow.css('background-color', '#DDC0B4'); // hightlights the searched row
+			if (record.includes(inputValue)) {
+				lastRow.css('background-color', '#DDC0B4'); // highlights the searched row
 			}
-			tbody.append(lastRow);
+			$("tbody").append(lastRow);
 		}
 	} catch (error) {
 		console.error("Error displaying records:", error);
-	}
-}
+	};
+};
 displayRecords()
 
 async function updateRecordsAndResize(inputValue: number) {
@@ -126,7 +141,7 @@ async function updateRecordsAndResize(inputValue: number) {
 	firstNumber = Math.max(0, inputValue - quarterRange);
 	lastNumber = Math.min(count, firstNumber + (calculatedRows - 1));
 	await displayRecords();
-}
+};
 
 $('#closeModalBtn').on("click", () => {
 	$('.modal').css('display', 'none') // closes modal
@@ -144,21 +159,21 @@ async function rightArrow(): Promise<void> {
 	let count = await fetchRecordCount() - 1;
 	if (lastRow) { // checks if the last row exists
 		const cells = lastRow.querySelectorAll("td");
-		const lastRecord: string[] = [];
-		cells.forEach((cell) => {
+		const lastRecord = [];
+		for (const cell of Array.from(cells)) {
 			lastRecord.push(cell.textContent || "");
-		});
+		}
 		const lastID = parseFloat(lastRecord[0]); // determines te value in the last row
 		if (0 <= lastID && lastID <= (count)) { // checks if the last value is within range
 			const tbody = $("tbody");
 			tbody.empty(); // empties the table
 			firstNumber = lastID + 1; // calculates the first number of the page
-			let calculatedRows = await adjustRowsByScreenHeight();
+			let calculatedRows = adjustRowsByScreenHeight();
 			lastNumber = firstNumber + (calculatedRows - 1) // calculates the first number of the page
 			await displayRecords(); // display the new records
-		}
-	}
-}
+		};
+	};
+};
 
 async function leftArrow(): Promise<void> {
 	$('#searchInput').val('')
@@ -166,18 +181,18 @@ async function leftArrow(): Promise<void> {
 	const firstRow = document.querySelector("#recordsTable tbody .row:first-child"); // retrieves the first row
 	if (firstRow) { // checks if the first row exists
 		const cells = firstRow.querySelectorAll("td");
-		const firstRecord: string[] = [];
-		cells.forEach((cell) => {
+		const firstRecord = [];
+		for (const cell of Array.from(cells)) {
 			firstRecord.push(cell.textContent || "");
-		});
+		}
 		const firstID = parseFloat(firstRecord[0]); // determines te value in the first row
 		if (0 <= firstID && firstID <= (count)) { // checks if the first value is within range
 			const tbody = $("tbody");
 			tbody.empty(); // empties the table
-			const calculatedRows = await adjustRowsByScreenHeight();
+			const calculatedRows = adjustRowsByScreenHeight();
 			lastNumber = firstID - 1; // calculates the last number of the page
 			firstNumber = lastNumber - (calculatedRows - 1) // uses the last number to calculate
 			await displayRecords();
-		}
-	}
-}
+		};
+	};
+};
