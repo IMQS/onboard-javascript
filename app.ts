ab121b86a0bb7500e4bfd5db604ef2ee828e82da
maxtf141 <isaacsmaxwill22@gmail.com>
2023-09-01 08:41:22 +0200

    Small changes

--- app.ts
+++ app.ts
@@ -24,7 +24,7 @@ async function getRecords(fromID: number, toID: number): Promise<string[][]> {
 	} catch (error) {
 		throw error;
 	};
-}
+};
 
 // This function  will handle retrieving the columns from the api
 async function getColumns(): Promise<string[]> {
@@ -49,7 +49,7 @@ async function getRecordCount(): Promise<number> {
 	} catch (error) {
 		throw error;
 		;
-	}
+	};
 };
 
 // This function will loop through and display the appropriate columns in the correct order.
@@ -75,7 +75,7 @@ async function showRecords(fromID: number, toID: number): Promise<void> {
 		let count = (await getRecordCount());
 		let inputNumber = input();
 		const maxRecords = recordsPerPage();
-		let condition = Math.ceil(count / maxRecords)
+		let condition = Math.ceil(count / maxRecords);
 		$("tbody").empty();
 		loader();
 		globals.currentToID = toID;
@@ -84,7 +84,7 @@ async function showRecords(fromID: number, toID: number): Promise<void> {
 			fromID = toID - (maxRecords - 1);
 		} else if (globals.currentPage === 1) {
 			fromID = 1;
-		}
+		};
 		let records = await getRecords(fromID, toID);
 		let stringCount = count.toLocaleString().replace(/,/g, " ");
 		$('.results').empty().append(`Displaying ID's ${fromID} - ${toID} out of ${stringCount}`);
@@ -94,7 +94,7 @@ async function showRecords(fromID: number, toID: number): Promise<void> {
 				$(".body-row:last-child").append(`<td><span>${records[i][n]}</span></td>`);
 			}
 			$("tbody").append(`</tr>`);
-		}
+		};
 
 		$("span").each(function () {
 			const lowercasedID = $(this).text();
@@ -107,12 +107,12 @@ async function showRecords(fromID: number, toID: number): Promise<void> {
 
 		if (condition >= fromID && condition <= toID) {
 			$(".next").css({ display: "none" });
-		}
+		};
 	} catch (error) {
 		throw error;
 	}
 	isFunctionRunning = false;
-}
+};
 
 // The following function handles all the functionality of the pagination and the pages. Including what records should be shown in the table.
 async function pageNumbers(start: number, end: number): Promise<void> {
@@ -131,7 +131,7 @@ async function pageNumbers(start: number, end: number): Promise<void> {
 			$(".next").css({ display: "none" });
 		} else {
 			$(".next").css({ display: "block" });
-		}
+		};
 		if (start < 1) {
 			start = 1;
 		};
@@ -141,7 +141,7 @@ async function pageNumbers(start: number, end: number): Promise<void> {
 			$(".pagination").append(
 				`<a id="${i}" class="pagination-item">${i}</a>`
 			);
-		}
+		};
 		if (globals.firstPage === 1) {
 			$(".prev").css({ display: "none" });
 		} else {
@@ -165,8 +165,6 @@ async function pageNumbers(start: number, end: number): Promise<void> {
 				globals.currentFromID = 1;
 			};
 			let toID = fromID + (maxRecords - 1);
-
-
 			if (fromID > count + 1 || toID > count + 1) {
 				toID = count;
 				fromID = toID - maxRecords;
@@ -177,10 +175,6 @@ async function pageNumbers(start: number, end: number): Promise<void> {
 			$(this).addClass("active");
 			isFunctionRunning = false;
 			await showRecords(fromID, toID);
-			// $(".results").empty();
-			// $(".results").append(
-			// 	`Displaying ID's ${fromID} - ${toID} out of ${stringCount}`
-			// );
 			return [fromID, toID];
 		});
 
@@ -201,7 +195,7 @@ async function pageNumbers(start: number, end: number): Promise<void> {
 	$(".next").on("click", async function () {
 		if (isFunctionRunning) {
 			return;
-		}
+		};
 		isFunctionRunning = true;
 		globals.firstPage = globals.lastPage + 1;
 		globals.lastPage = globals.firstPage + 9;
@@ -214,7 +208,7 @@ async function pageNumbers(start: number, end: number): Promise<void> {
 	$(".prev").on("click", async function () {
 		if (isFunctionRunning) {
 			return;
-		}
+		};
 		isFunctionRunning = true;
 		globals.lastPage = globals.firstPage - 1;
 		globals.firstPage = globals.lastPage - 9;
@@ -222,23 +216,23 @@ async function pageNumbers(start: number, end: number): Promise<void> {
 		await pageNumbers(globals.firstPage, globals.lastPage);
 		isFunctionRunning = false;
 	});
-}
+};
 
+// Event listener to prevent some characters to be entered in the input
 $('.search-input').on('keydown', function (e) {
 	if (e.key === 'e'|| e.key ===  'E'|| e.key ===  '.' || e.key ===  '+' || e.key ===  '*'|| e.key ===  '-'){
 		e.preventDefault();
 	}
 	if (e.key === 'Enter') {
 		$('.results-box').trigger('click')
-		e.key
-	}
+	};
 });
 
 // In this function wil do the extract the number entered in the search. Then it would take that and calculate the range which should be displayed for the user to click on. 
 $(".search-input").on("input", async function (this: HTMLInputElement, event: any) {
 	if (isFunctionRunning) {
 		return;
-	}
+	};
 	isFunctionRunning = true;
 	event.preventDefault();
 	let count = await getRecordCount();
@@ -275,7 +269,7 @@ $(".search-input").on("input", async function (this: HTMLInputElement, event: an
 	};
 
 	isFunctionRunning = false;
-})
+});
 
 // After the range has been returned to the user. The user can click on it and that will show the range of records on the table. 
 async function resultsSelect(event: any) {
@@ -283,7 +277,6 @@ async function resultsSelect(event: any) {
 		return;
 	};
 	isFunctionRunning = true;
-	console.log(event.key)
 	let count = await getRecordCount();
 	let idRange = $('.results-select').text();
 	let rangeArray = null;
@@ -295,24 +288,25 @@ async function resultsSelect(event: any) {
 	globals.currentPage = Math.ceil(endID / maxRecords);
 	let pageEnd = Math.ceil(globals.currentPage / 10) * 10;
 	let pageStart = pageEnd - 9;
-
 	if (endID === count) {
 		startID = ((globals.currentPage - 1) * maxRecords) + 1;
 		endID = count;
 		globals.firstPage = pageStart;
 		globals.lastPage = pageEnd;
 	};
-
 	await pageNumbers(pageStart, pageEnd);
 	await showRecords(startID, endID);
-	isFunctionRunning = false;;
-
-}
+	isFunctionRunning = false;
+};
 
 
 
 // When adjusting the height and on different screen sizes. This function would responsible for calculating how much records should be displayed based on the height of the window itself. 
-async function adjustDisplayedRecords(): Promise<number> {
+async function adjustDisplayedRecords() {
+	if(isFunctionRunning){
+		return;
+	}
+	isFunctionRunning = true;
 	try {
 		let screenHeight = $(window).height() as number;
 		if (screenHeight < 68) {
@@ -323,7 +317,6 @@ async function adjustDisplayedRecords(): Promise<number> {
 		let inputNumber = input();
 		let length = inputNumber.length as number;
 		let inputNumberInt = parseInt(inputNumber);
-
 		if (inputNumber === '') {
 			let newCurrentPage = Math.ceil(globals.currentFromID / maxRecords);
 			if (newCurrentPage === 1) {
@@ -331,7 +324,7 @@ async function adjustDisplayedRecords(): Promise<number> {
 			};
 			globals.currentToID = globals.currentFromID + (maxRecords - 1);
 			globals.currentPage = newCurrentPage;
-			let originalID = (Math.floor(maxRecords * globals.currentPage) - (maxRecords - 1))
+			let originalID = (Math.floor(maxRecords * globals.currentPage) - (maxRecords - 1));
 			globals.difference = globals.currentFromID - originalID;
 		} else {
 			if (length > 0) {
@@ -342,13 +335,14 @@ async function adjustDisplayedRecords(): Promise<number> {
 				globals.currentToID = newCurrentPage * maxRecords;
 				globals.currentPage = newCurrentPage;
 				globals.currentFromID = (globals.currentPage - 1) * maxRecords + 1;
-			}
-		}
+			};
+		};
 		let pageEnd = Math.ceil(Math.floor(globals.currentToID / maxRecords) / 10) * 10;
 		let pageStart = pageEnd - 9;
 		globals.firstPage = pageStart;
 		globals.lastPage = pageEnd;
 		$("tbody").empty();
+		isFunctionRunning = false;
 		await showRecords(globals.currentFromID, globals.currentToID);
 		await pageNumbers(pageStart, pageEnd);
 		return maxRecords;
@@ -364,7 +358,7 @@ function resize() {
 	resizeTimer = setTimeout(async () => {
 		await adjustDisplayedRecords();
 	}, 250);
-}
+};
 
 // Just a loader to display when the table is empty and records is being fetched. 
 function loader() {
@@ -372,7 +366,7 @@ function loader() {
 	if (content === '') {
 		$('.results').append('<div class="loader"></div>');
 	} else {
-		$('.loader').css({ 'display': 'none' })
+		$('.loader').css({ 'display': 'none' });
 	};
 };
 
