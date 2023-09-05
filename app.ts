10b0d4086e03e7fcc17f9c50745a995b671dbe51
ThokozaniNqwili <simthokozile@gmail.com>
2023-09-05 08:12:37 +0200

    09/05

--- app.ts
+++ app.ts
@@ -1,75 +1,70 @@
 import { ajax, css } from "jquery";
-let firstNumber = 0;
-let lastNumber = 0;
-let backend: string = "http://localhost:2050";
+class GlobalVariables {
+	firstNumber = 0;
+	lastNumber = 0;
+	backend = "http://localhost:2050";
 
-window.onload = () => {
-	createTable();
-	displayRecords();
-	$('#btnSearch').on("click", async (event) => {
-		event.preventDefault();
-		let inputValue = $('#searchInput').val() as number;
-		await updateRecordsAndResize(inputValue); // calls to calculate the range once button is clicked
-	});
-	$('#closeModalBtn').on("click", () => {
-		$('.content').empty()
-		$('.modal').css('display', 'none') // closes modal
-	});
-	$('.arrow-right').on('click', () => {
-		rightArrow();
-	});
-	$('.arrow-left').on('click', () => {
-		leftArrow();
-	});
-	$('#searchInput').on('keydown', (event) => {
-		if (event.key === 'e' || event.key === 'E') {
-			event.preventDefault();
-		};
-	});
-	$('#searchInput').on('input', (event) => {
-		const inputValue = $('#searchInput').val() as string;
-		if (inputValue.includes('.')) {
-			$('#searchInput').val(inputValue.replace('.', ' '));
-		};
-	});
-};
+	async fetchRecordCount(): Promise<number> {
+		return fetch(`${this.backend}/recordCount`)
+			.then((res) => {
+				if (!res.ok) {
+					throw 'Failed to fetch record count';
+				}
+				return res.json();
+			})
+			.catch((err) => {
+				throw 'Error fetching the record count: ' + err;
+			});
+	}
 
-async function fetchRecordCount(): Promise<number> {
-	try {
-		const response = await fetch(`${backend}/recordCount`);
-		if (!response.ok) {
-			throw new Error('Failed to fetch record count');
-		}
-		return response.json()
-	} catch (error) {
-		throw new Error('Error fetching the record count')
-	};
-};
+	async fetchColumns(): Promise<string[]> {
+		return fetch(`${this.backend}/columns`)
+			.then(res => {
+				if (!res.ok) {
+					throw 'Failed to fetch columns';
+				}
+				return res.json();
+			}).catch(err => {
+				throw 'Error fetching columns' + err;
+			})
+	}
 
-async function fetchColumns(): Promise<string[]> {
-	try {
-		const response = await fetch(`${backend}/columns`);
-		if (!response.ok) {
-			throw new Error('Failed to fetch columns');
-		}
-		const jsonText = await response.text();
-		const columns: string[] = await JSON.parse(jsonText);
-		return columns
-	} catch (error) {
-		throw new Error('Error fetching columns')
-	};
-};
+	async fetchRecords(from: number, to: number): Promise<any[]> {
+		return fetch(`${this.backend}/records?from=${from}&to=${to}`)
+			.then(res => {
+				if (!res.ok) {
+					throw "Sorry, there's a problem with the network";
+				}
+				return res.json();
+			}).catch(err => {
+				throw 'Error fetching records from server ' + err;
+			});
+	}
+}
+
+const globalVars = new GlobalVariables();
+let recordCount: number;
+
+globalVars.fetchRecordCount()
+	.then((count) => {
+		recordCount = count - 1; // Assign the fetched count to the global variable
+		displayRecords(recordCount);
+	})
+	.catch(err => {
+		throw 'Error fetching record count' + err;
+	})
 
 async function createTable() {
-	try {
-		const columns = await fetchColumns();
-		for (const col of columns) {
-			$(".head").append(`<th>${col}</th>`);
-		};
-	} catch (error) {
-		throw new Error('Error creating table');
-	};
-};
+	return globalVars.fetchColumns()
+		.then(columns => {
+			for (const col of columns) {
+				$(".head").append(`<th>${col}</th>`);
+			}
+		})
+		.catch(err => {
+			throw 'Error creating table' + err;
+		})
+}
 
 function adjustRowsByScreenHeight() {
 	const screenHeight = window.innerHeight;
@@ -80,123 +75,141 @@ function adjustRowsByScreenHeight() {
 	} else {
 		let maxRows = Math.floor(availableHeight / rowHeight);
 		return maxRows;
-	};
-};
+	}
+}
 
 $(window).on('resize', async () => {
 	let resizeTimeout: number;
 	resizeTimeout = setTimeout(async () => {
-		$('#loader').show()
-		await displayRecords()
+		$('#loader').show();
+		await displayRecords(recordCount);
 		let inputValue = $('#searchInput').val(); // priotizes the search input value if available
 		if (inputValue !== '') {
-			await updateRecordsAndResize(Number(inputValue))
+			await updateRecordsAndResize(Number(inputValue));
 		}
 		$('#loader').hide();
-	}, 500);
-});
+	}, 500)
+})
 
-async function fetchRecords(from: number, to: number): Promise<any[]> {
-	try {
-		const response = await fetch(`${backend}/records?from=${from}&to=${to}`);
-		if (!response.ok) {
-			throw new Error("Sorry, there's a problem with the network");
-		}
-		return response.json();
-	} catch (error) {
-		throw new Error('Error fetching records from server');
-	};
-};
-
-async function displayRecords(): Promise<void> {
-	try {
-		$('#loader').show();
-		let count = await fetchRecordCount() - 1;
-		let calculatedRows = adjustRowsByScreenHeight();
-		const inputValue = $('#searchInput').val() as number;
-		if (calculatedRows === 0) {
-			lastNumber = firstNumber
-		} else if (firstNumber < 0) {
-			firstNumber = 0;
-			lastNumber = firstNumber + (calculatedRows - 1);
+async function displayRecords(recordCount: number): Promise<void> {
+	$('#loader').show();
+	let calculatedRows = adjustRowsByScreenHeight();
+	const inputValue = $('#searchInput').val() as number;
+	if (calculatedRows === 0) {
+		globalVars.lastNumber = globalVars.firstNumber;
+	} else if (globalVars.firstNumber < 0) {
+		globalVars.firstNumber = 0;
+		globalVars.lastNumber = globalVars.firstNumber + (calculatedRows - 1);
+	} else {
+		globalVars.lastNumber = globalVars.firstNumber + (calculatedRows - 1);
+	}
+	if (globalVars.firstNumber === 0) {
+		$('.arrow-left').hide();
+	} else {
+		$('.arrow-left').show();
+	}
+	if (globalVars.lastNumber >= recordCount) {
+		$('.arrow-right').hide();
+	} else {
+		$('.arrow-right').show();
+	}
+	if (!isNaN(globalVars.firstNumber) && !isNaN(globalVars.lastNumber) && !isNaN(recordCount) && !isNaN(calculatedRows)) {
+		if (globalVars.lastNumber <= recordCount && globalVars.lastNumber >= 0) {
+			return globalVars
+				.fetchRecords(globalVars.firstNumber, globalVars.lastNumber)
+				.then((records) => {
+					$('#page').empty();
+					$('#page').append(`Showing record: ${globalVars.firstNumber} - ${globalVars.lastNumber}`);
+					$('#loader').hide();
+					const tbody = $("tbody");
+					tbody.empty();
+					for (const record of records) {
+						$("tbody").append(`<tr class="row"></tr>`); // creates row for each record
+						const lastRow = $(".row:last");
+						for (const value of record) {
+							lastRow.append(`<td>${value}</td>`); // assign each record to their column in a specified row
+						}
+						if (record.includes(inputValue)) {
+							lastRow.css('background-color', '#DDC0B4'); // highlights the searched row
+						}
+						$("tbody").append(lastRow);
+					}
+				})
+				.catch((err) => {
+					throw 'Error fetching records' + err;
+				});
 		} else {
-			lastNumber = firstNumber + (calculatedRows - 1);
-		};
-		let records;
-		if (lastNumber <= count && lastNumber >= 0) {
-			records = await fetchRecords(firstNumber, lastNumber);
-			$('#page').empty();
-			$('#page').append(`Showing record: ${firstNumber} - ${lastNumber}`); // changes the record range showing
-			$('#loader').hide();
-		} else {
-			firstNumber = count - (calculatedRows - 1);
-			lastNumber = count;
-			records = await fetchRecords(firstNumber, count);
-			$('#page').empty();
-			$('#page').append(`Showing record: ${firstNumber} - ${count}`);
-			$('#loader').hide();
-		};
-		const tbody = $("tbody");
-		tbody.empty();
-		for (const record of records) {
-			$("tbody").append(`<tr class="row"></tr>`); // creates row for each record
-			const lastRow = $(".row:last");
-			for (const value of record) {
-				lastRow.append(`<td>${value}</td>`); // assign each record to their column in a specified row
-			}
-			if (record.includes(inputValue)) {
-				lastRow.css('background-color', '#DDC0B4'); // highlights the searched row
-			}
-			$("tbody").append(lastRow);
+			globalVars.firstNumber = recordCount - (calculatedRows - 1);
+			globalVars.lastNumber = recordCount;
+			return globalVars
+				.fetchRecords(globalVars.firstNumber, recordCount)
+				.then((records) => {
+					$('#page').empty();
+					$('#page').append(`Showing record: ${globalVars.firstNumber} - ${globalVars.lastNumber}`);
+					$('#loader').hide();
+					const tbody = $("tbody");
+					tbody.empty();
+					for (const record of records) {
+						$("tbody").append(`<tr class="row"></tr>`); // creates row for each record
+						const lastRow = $(".row:last");
+						for (const value of record) {
+							lastRow.append(`<td>${value}</td>`); // assign each record to their column in a specified row
+						}
+						if (record.includes(inputValue)) {
+							lastRow.css('background-color', '#DDC0B4'); // highlights the searched row
+						}
+						$("tbody").append(lastRow);
+						$('#loader').hide();
+					}
+				})
+				.catch((err) => {
+					throw 'Error fetching records' + err;
+				})
 		}
-	} catch (error) {
-		throw new Error('Error displaying records');
-	};
-};
+	} else {
+		throw new Error("Invalid numeric values detected.");
+	}
+}
 
 async function updateRecordsAndResize(inputValue: number) {
-	let count = await fetchRecordCount() - 1;
-	if (inputValue < 0 || inputValue > count) { // check if the search input 
+	if (inputValue < 0 || inputValue > recordCount) { // check if the search input 
 		$('.modal').css('display', 'block');//opens modal if search input is not within range
-		$('.content').append(`<p>${inputValue} is not a number within the range.Please try a different number</p>`);
+		$('.content').append(`<p>${inputValue} is not a number within the range. Please try a different number</p>`);
 		$('#searchInput').val(''); // empties search bar
-		return;
-	};
+		return
+	}
 	let calculatedRows = adjustRowsByScreenHeight();
 	const halfRange = Math.floor(calculatedRows / 2); // divides the calculated max rows in half 
-	firstNumber = Math.max(0, inputValue - halfRange);
-	lastNumber = Math.min(count, firstNumber + (calculatedRows - 1));
-	await displayRecords();
-};
+	globalVars.firstNumber = Math.max(0, inputValue - halfRange);
+	globalVars.lastNumber = Math.min(recordCount, globalVars.firstNumber + (calculatedRows - 1));
+	await displayRecords(recordCount);
+}
 
 async function rightArrow(): Promise<void> {
-	$('.arrow-right').css('display', 'none')
+	$('#page').empty();
 	$('#searchInput').val('');
 	const lastRow = document.querySelector("#recordsTable tbody .row:last-child"); // retrieves the last row
-	let count = await fetchRecordCount() - 1;
 	if (lastRow) { // checks if the last row exists
 		const cells = lastRow.querySelectorAll("td");
 		const lastRecord = [];
 		for (const cell of Array.from(cells)) {
 			lastRecord.push(cell.textContent || "");
-		};
+		}
 		const lastID = parseFloat(lastRecord[0]); // determines te value in the last row
-		if (0 <= lastID && lastID <= (count)) { // checks if the last value is within range
+		if (0 <= lastID && lastID <= (recordCount)) { // checks if the last value is within range
 			const tbody = $("tbody");
 			tbody.empty(); // empties the table
-			firstNumber = lastID + 1; // calculates the first number of the page
+			globalVars.firstNumber = lastID + 1; // calculates the first number of the page
 			let calculatedRows = adjustRowsByScreenHeight();
-			lastNumber = firstNumber + (calculatedRows - 1);// calculates the first number of the page
-			await displayRecords(); // display the new records
-			$('.arrow-right').css('display', 'inline-block')
-		};
-	};
-};
+			globalVars.lastNumber = globalVars.firstNumber + (calculatedRows - 1);// calculates the first number of the page
+			await displayRecords(recordCount); // display the new records
+		}
+	}
+}
 
 async function leftArrow(): Promise<void> {
-	$('.arrow-left').css('display', 'none')
+	$('#page').empty();
 	$('#searchInput').val('');
-	let count = await fetchRecordCount() - 1;
 	const firstRow = document.querySelector("#recordsTable tbody .row:first-child"); // retrieves the first row
 	if (firstRow) { // checks if the first row exists
 		const cells = firstRow.querySelectorAll("td");
@@ -205,14 +218,45 @@ async function leftArrow(): Promise<void> {
 			firstRecord.push(cell.textContent || "");
 		};
 		const firstID = parseFloat(firstRecord[0]); // determines te value in the first row
-		if (0 <= firstID && firstID <= (count)) { // checks if the first value is within range
+		if (0 <= firstID && firstID <= (recordCount)) { // checks if the first value is within range
 			const tbody = $("tbody");
 			tbody.empty(); // empties the table
 			const calculatedRows = adjustRowsByScreenHeight();
-			lastNumber = firstID - 1; // calculates the last number of the page
-			firstNumber = lastNumber - (calculatedRows - 1); // uses the last number to calculate
-			await displayRecords();
-			$('.arrow-left').css('display', 'inline-block')
-		};
-	};
-};
+			globalVars.lastNumber = firstID - 1; // calculates the last number of the page
+			globalVars.firstNumber = globalVars.lastNumber - (calculatedRows - 1); // uses the last number to calculate
+			await displayRecords(recordCount);
+		}
+	}
+}
+
+window.onload = () => {
+	createTable();
+	displayRecords(recordCount);
+	$('#btnSearch').on("click", async (event) => {
+		event.preventDefault();
+		let inputValue = $('#searchInput').val() as number;
+		$('#page').empty();
+		await updateRecordsAndResize(inputValue); // calls to calculate the range once button is clicked
+	})
+	$('#closeModalBtn').on("click", () => {
+		$('.content').empty();
+		$('.modal').css('display', 'none'); // closes modal
+	})
+	$('.arrow-right').on('click', () => {
+		rightArrow();
+	})
+	$('.arrow-left').on('click', () => {
+		leftArrow();
+	})
+	$('#searchInput').on('keydown', (event) => {
+		if (event.key === 'e' || event.key === 'E') {
+			event.preventDefault();
+		}
+	})
+	$('#searchInput').on('input', (event) => {
+		const inputValue = $('#searchInput').val() as string;
+		if (inputValue.includes('.')) {
+			$('#searchInput').val(inputValue.replace('.', ''));
+		}
+	})
+}
